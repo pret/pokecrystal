9ab98276409a816dda1e4bf5cf54b693ac57a6f2
Bryan Bishop <kanzure@gmail.com>
2012-04-20 11:51:14 -0500

    replace $4000 with end-of-bank calculations

--- main.asm
+++ main.asm
@@ -246,17 +246,17 @@ INCBIN "baserom.gbc",$c224,$10000-$c224
 
 
 SECTION "bank4",DATA,BANK[$4]
-INCBIN "baserom.gbc",$10000,$4000
+INCBIN "baserom.gbc",$10000,$14000 - $10000
 SECTION "bank5",DATA,BANK[$5]
-INCBIN "baserom.gbc",$14000,$4000
+INCBIN "baserom.gbc",$14000,$18000 - $14000
 SECTION "bank6",DATA,BANK[$6]
-INCBIN "baserom.gbc",$18000,$4000
+INCBIN "baserom.gbc",$18000,$1C000 - $18000
 SECTION "bank7",DATA,BANK[$7]
-INCBIN "baserom.gbc",$1C000,$4000
+INCBIN "baserom.gbc",$1C000,$20000 - $1C000
 SECTION "bank8",DATA,BANK[$8]
-INCBIN "baserom.gbc",$20000,$4000
+INCBIN "baserom.gbc",$20000,$24000 - $20000
 SECTION "bank9",DATA,BANK[$9]
-INCBIN "baserom.gbc",$24000,$4000
+INCBIN "baserom.gbc",$24000,$28000 - $24000
 SECTION "bankA",DATA,BANK[$A]
 INCBIN "baserom.gbc",$28000,$2A5e9 - $28000
 
@@ -3183,15 +3183,15 @@ WildMons6: ; 0x2b92f
 INCBIN "baserom.gbc",$2b930,$2c000 - $2b930
 
 SECTION "bankB",DATA,BANK[$B]
-INCBIN "baserom.gbc",$2C000,$4000
+INCBIN "baserom.gbc",$2C000,$30000 - $2C000
 SECTION "bankC",DATA,BANK[$C]
-INCBIN "baserom.gbc",$30000,$4000
+INCBIN "baserom.gbc",$30000,$34000 - $30000
 SECTION "bankD",DATA,BANK[$D]
-INCBIN "baserom.gbc",$34000,$4000
+INCBIN "baserom.gbc",$34000,$38000 - $34000
 SECTION "bankE",DATA,BANK[$E]
-INCBIN "baserom.gbc",$38000,$4000
+INCBIN "baserom.gbc",$38000,$3C000 - $38000
 SECTION "bankF",DATA,BANK[$F]
-INCBIN "baserom.gbc",$3C000,$4000
+INCBIN "baserom.gbc",$3C000,$40000 - $3C000
 SECTION "bank10",DATA,BANK[$10]
 INCBIN "baserom.gbc",$40000,$1afb
 
@@ -3454,11 +3454,11 @@ Move1:
 INCBIN "baserom.gbc",$421d8,$44000-$421d8
 
 SECTION "bank11",DATA,BANK[$11]
-INCBIN "baserom.gbc",$44000,$4000
+INCBIN "baserom.gbc",$44000,$48000 - $44000
 SECTION "bank12",DATA,BANK[$12]
-INCBIN "baserom.gbc",$48000,$4000
+INCBIN "baserom.gbc",$48000,$4C000 - $48000
 SECTION "bank13",DATA,BANK[$13]
-INCBIN "baserom.gbc",$4C000,$4000
+INCBIN "baserom.gbc",$4C000,$50000 - $4C000
 SECTION "bank14",DATA,BANK[$14]
 INCBIN "baserom.gbc",$50000,$3384
 
@@ -3719,11 +3719,11 @@ INCBIN "baserom.gbc",$50000,$3384
 	db "?????@@@@@"
 	db "?????@@@@@"
 
-INCBIN "baserom.gbc",$53D84,$4000 - $3D84
+INCBIN "baserom.gbc",$53D84,$54000 - $53D84
 SECTION "bank15",DATA,BANK[$15]
-INCBIN "baserom.gbc",$54000,$4000
+INCBIN "baserom.gbc",$54000,$58000 - $54000
 SECTION "bank16",DATA,BANK[$16]
-INCBIN "baserom.gbc",$58000,$4000
+INCBIN "baserom.gbc",$58000,$5C000 - $58000
 SECTION "bank17",DATA,BANK[$17]
 INCBIN "baserom.gbc",$5C000,$4000
 SECTION "bank18",DATA,BANK[$18]
