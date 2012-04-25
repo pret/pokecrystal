9a3733e8d1c29afd75173c9dbd58fc1b4327b790
Bryan Bishop <kanzure@gmail.com>
2012-04-25 18:33:36 -0500

    run main.asm through the line spacing logic once

--- main.asm
+++ main.asm
@@ -1,4 +1,5 @@
 SECTION "bank0",HOME
+
 INCBIN "baserom.gbc",$0,$304d
 
 GetFarByte: ; 0x304d
@@ -35,9 +36,13 @@ AddNTimes: ; 0x30fe
 ; 0x3105
 
 INCBIN "baserom.gbc",$3105,$4000-$3105
+
 SECTION "bank1",DATA,BANK[$1]
+
 INCBIN "baserom.gbc",$4000,$4000
+
 SECTION "bank2",DATA,BANK[$2]
+
 INCBIN "baserom.gbc",$8000,$a68
 
 CheckShininess: ; 0x8a68
@@ -69,6 +74,7 @@ CheckShininess: ; 0x8a68
 INCBIN "baserom.gbc",$8a88,$c000-$8a88
 
 SECTION "bank3",DATA,BANK[$3]
+
 INCBIN "baserom.gbc",$c000,$29
 
 SpecialsPointers: ; 0xc029
@@ -244,20 +250,32 @@ SpecialsPointers: ; 0xc029
 
 INCBIN "baserom.gbc",$c224,$10000-$c224
 
-
 SECTION "bank4",DATA,BANK[$4]
+
 INCBIN "baserom.gbc",$10000,$14000 - $10000
+
 SECTION "bank5",DATA,BANK[$5]
+
 INCBIN "baserom.gbc",$14000,$18000 - $14000
+
 SECTION "bank6",DATA,BANK[$6]
+
 INCBIN "baserom.gbc",$18000,$1C000 - $18000
+
 SECTION "bank7",DATA,BANK[$7]
+
 INCBIN "baserom.gbc",$1C000,$20000 - $1C000
+
 SECTION "bank8",DATA,BANK[$8]
+
 INCBIN "baserom.gbc",$20000,$24000 - $20000
+
 SECTION "bank9",DATA,BANK[$9]
+
 INCBIN "baserom.gbc",$24000,$28000 - $24000
+
 SECTION "bankA",DATA,BANK[$A]
+
 INCBIN "baserom.gbc",$28000,$2A5e9 - $28000
 
 WildMons1: ; 0x2a5e9
@@ -1912,7 +1930,6 @@ WildMons1: ; 0x2a5e9
 
 	db $FF
 
-
 WildMons2: ; 0x2b11d
 ; Johto Pokémon in water
 
@@ -3110,7 +3127,6 @@ WildMons4: ; 0x2b7f7
 
 	db $FF
 
-
 WildMons5: ; 0x2b8d0
 ; swarms in grass
 
@@ -3172,27 +3188,36 @@ WildMons5: ; 0x2b8d0
 
 	db $FF
 
-
 WildMons6: ; 0x2b92f
 ; swarms in water
 
 	; no swarms encountered while surfing in Crystal
 	db $FF
 
-
 INCBIN "baserom.gbc",$2b930,$2c000 - $2b930
 
 SECTION "bankB",DATA,BANK[$B]
+
 INCBIN "baserom.gbc",$2C000,$30000 - $2C000
+
 SECTION "bankC",DATA,BANK[$C]
+
 INCBIN "baserom.gbc",$30000,$34000 - $30000
+
 SECTION "bankD",DATA,BANK[$D]
+
 INCBIN "baserom.gbc",$34000,$38000 - $34000
+
 SECTION "bankE",DATA,BANK[$E]
+
 INCBIN "baserom.gbc",$38000,$3C000 - $38000
+
 SECTION "bankF",DATA,BANK[$F]
+
 INCBIN "baserom.gbc",$3C000,$40000 - $3C000
+
 SECTION "bank10",DATA,BANK[$10]
+
 INCBIN "baserom.gbc",$40000,$1afb
 
 Moves: ; 0x41afb
@@ -3454,12 +3479,19 @@ Move1:
 INCBIN "baserom.gbc",$421d8,$44000-$421d8
 
 SECTION "bank11",DATA,BANK[$11]
+
 INCBIN "baserom.gbc",$44000,$48000 - $44000
+
 SECTION "bank12",DATA,BANK[$12]
+
 INCBIN "baserom.gbc",$48000,$4C000 - $48000
+
 SECTION "bank13",DATA,BANK[$13]
+
 INCBIN "baserom.gbc",$4C000,$50000 - $4C000
+
 SECTION "bank14",DATA,BANK[$14]
+
 INCBIN "baserom.gbc",$50000,$3384
 
 	db "BULBASAUR@"
@@ -3720,34 +3752,61 @@ INCBIN "baserom.gbc",$50000,$3384
 	db "?????@@@@@"
 
 INCBIN "baserom.gbc",$53D84,$54000 - $53D84
+
 SECTION "bank15",DATA,BANK[$15]
+
 INCBIN "baserom.gbc",$54000,$58000 - $54000
+
 SECTION "bank16",DATA,BANK[$16]
+
 INCBIN "baserom.gbc",$58000,$5C000 - $58000
+
 SECTION "bank17",DATA,BANK[$17]
+
 INCBIN "baserom.gbc",$5C000,$4000
+
 SECTION "bank18",DATA,BANK[$18]
+
 INCBIN "baserom.gbc",$60000,$4000
+
 SECTION "bank19",DATA,BANK[$19]
+
 INCBIN "baserom.gbc",$64000,$4000
+
 SECTION "bank1A",DATA,BANK[$1A]
+
 INCBIN "baserom.gbc",$68000,$4000
+
 SECTION "bank1B",DATA,BANK[$1B]
+
 INCBIN "baserom.gbc",$6C000,$4000
+
 SECTION "bank1C",DATA,BANK[$1C]
+
 INCBIN "baserom.gbc",$70000,$4000
+
 SECTION "bank1D",DATA,BANK[$1D]
+
 INCBIN "baserom.gbc",$74000,$4000
+
 SECTION "bank1E",DATA,BANK[$1E]
+
 INCBIN "baserom.gbc",$78000,$4000
+
 SECTION "bank1F",DATA,BANK[$1F]
+
 INCBIN "baserom.gbc",$7C000,$4000
+
 SECTION "bank20",DATA,BANK[$20]
+
 INCBIN "baserom.gbc",$80000,$4000
+
 SECTION "bank21",DATA,BANK[$21]
+
 INCBIN "baserom.gbc",$84000,$4000
 
 SECTION "bank22",DATA,BANK[$22]
+
 INCBIN "baserom.gbc",$88000,$3170
 
 SpecialDratini: ; 0x8b170
@@ -3836,7 +3895,6 @@ SpecialDratini: ; 0x8b170
 	db TWISTER
 	db 0
 
-
 GetNthPartyMon: ; 0x8b1ce
 ; inputs:
 ; hl must be set to 0 before calling this function.
@@ -3865,10 +3923,15 @@ GetNthPartyMon: ; 0x8b1ce
 INCBIN "baserom.gbc",$8b1e1,$8c000-$8b1e1
 
 SECTION "bank23",DATA,BANK[$23]
+
 INCBIN "baserom.gbc",$8C000,$4000
+
 SECTION "bank24",DATA,BANK[$24]
+
 INCBIN "baserom.gbc",$90000,$4000
+
 SECTION "bank25",DATA,BANK[$25]
+
 INCBIN "baserom.gbc",$94000,$94034 - $94000
 
 MapHeader_0x94034: ; 0x94034
@@ -3887,157 +3950,309 @@ MapHeader_0x94034: ; 0x94034
 INCBIN "baserom.gbc",$9403d,$98000 - $9403d
 
 SECTION "bank26",DATA,BANK[$26]
+
 INCBIN "baserom.gbc",$98000,$4000
+
 SECTION "bank27",DATA,BANK[$27]
+
 INCBIN "baserom.gbc",$9C000,$4000
+
 SECTION "bank28",DATA,BANK[$28]
+
 INCBIN "baserom.gbc",$A0000,$4000
+
 SECTION "bank29",DATA,BANK[$29]
+
 INCBIN "baserom.gbc",$A4000,$4000
+
 SECTION "bank2A",DATA,BANK[$2A]
+
 INCBIN "baserom.gbc",$A8000,$4000
+
 SECTION "bank2B",DATA,BANK[$2B]
+
 INCBIN "baserom.gbc",$AC000,$4000
+
 SECTION "bank2C",DATA,BANK[$2C]
+
 INCBIN "baserom.gbc",$B0000,$4000
+
 SECTION "bank2D",DATA,BANK[$2D]
+
 INCBIN "baserom.gbc",$B4000,$4000
+
 SECTION "bank2E",DATA,BANK[$2E]
+
 INCBIN "baserom.gbc",$B8000,$4000
+
 SECTION "bank2F",DATA,BANK[$2F]
+
 INCBIN "baserom.gbc",$BC000,$4000
+
 SECTION "bank30",DATA,BANK[$30]
+
 INCBIN "baserom.gbc",$C0000,$4000
+
 SECTION "bank31",DATA,BANK[$31]
+
 INCBIN "baserom.gbc",$C4000,$4000
+
 SECTION "bank32",DATA,BANK[$32]
+
 INCBIN "baserom.gbc",$C8000,$4000
+
 SECTION "bank33",DATA,BANK[$33]
+
 INCBIN "baserom.gbc",$CC000,$4000
+
 SECTION "bank34",DATA,BANK[$34]
+
 INCBIN "baserom.gbc",$D0000,$4000
+
 SECTION "bank35",DATA,BANK[$35]
+
 INCBIN "baserom.gbc",$D4000,$4000
+
 SECTION "bank36",DATA,BANK[$36]
+
 INCBIN "baserom.gbc",$D8000,$4000
+
 SECTION "bank37",DATA,BANK[$37]
+
 INCBIN "baserom.gbc",$DC000,$4000
+
 SECTION "bank38",DATA,BANK[$38]
+
 INCBIN "baserom.gbc",$E0000,$4000
+
 SECTION "bank39",DATA,BANK[$39]
+
 INCBIN "baserom.gbc",$E4000,$4000
+
 SECTION "bank3A",DATA,BANK[$3A]
+
 INCBIN "baserom.gbc",$E8000,$4000
+
 SECTION "bank3B",DATA,BANK[$3B]
+
 INCBIN "baserom.gbc",$EC000,$4000
+
 SECTION "bank3C",DATA,BANK[$3C]
+
 INCBIN "baserom.gbc",$F0000,$4000
+
 SECTION "bank3D",DATA,BANK[$3D]
+
 INCBIN "baserom.gbc",$F4000,$4000
+
 SECTION "bank3E",DATA,BANK[$3E]
+
 INCBIN "baserom.gbc",$F8000,$4000
+
 SECTION "bank3F",DATA,BANK[$3F]
+
 INCBIN "baserom.gbc",$FC000,$4000
+
 SECTION "bank40",DATA,BANK[$40]
+
 INCBIN "baserom.gbc",$100000,$4000
+
 SECTION "bank41",DATA,BANK[$41]
+
 INCBIN "baserom.gbc",$104000,$4000
+
 SECTION "bank42",DATA,BANK[$42]
+
 INCBIN "baserom.gbc",$108000,$4000
+
 SECTION "bank43",DATA,BANK[$43]
+
 INCBIN "baserom.gbc",$10C000,$4000
+
 SECTION "bank44",DATA,BANK[$44]
+
 INCBIN "baserom.gbc",$110000,$4000
+
 SECTION "bank45",DATA,BANK[$45]
+
 INCBIN "baserom.gbc",$114000,$4000
+
 SECTION "bank46",DATA,BANK[$46]
+
 INCBIN "baserom.gbc",$118000,$4000
+
 SECTION "bank47",DATA,BANK[$47]
+
 INCBIN "baserom.gbc",$11C000,$4000
+
 SECTION "bank48",DATA,BANK[$48]
+
 INCBIN "baserom.gbc",$120000,$4000
+
 SECTION "bank49",DATA,BANK[$49]
+
 INCBIN "baserom.gbc",$124000,$4000
+
 SECTION "bank4A",DATA,BANK[$4A]
+
 INCBIN "baserom.gbc",$128000,$4000
+
 SECTION "bank4B",DATA,BANK[$4B]
+
 INCBIN "baserom.gbc",$12C000,$4000
+
 SECTION "bank4C",DATA,BANK[$4C]
+
 INCBIN "baserom.gbc",$130000,$4000
+
 SECTION "bank4D",DATA,BANK[$4D]
+
 INCBIN "baserom.gbc",$134000,$4000
+
 SECTION "bank4E",DATA,BANK[$4E]
+
 INCBIN "baserom.gbc",$138000,$4000
+
 SECTION "bank4F",DATA,BANK[$4F]
+
 INCBIN "baserom.gbc",$13C000,$4000
+
 SECTION "bank50",DATA,BANK[$50]
+
 INCBIN "baserom.gbc",$140000,$4000
+
 SECTION "bank51",DATA,BANK[$51]
+
 INCBIN "baserom.gbc",$144000,$4000
+
 SECTION "bank52",DATA,BANK[$52]
+
 INCBIN "baserom.gbc",$148000,$4000
+
 SECTION "bank53",DATA,BANK[$53]
+
 INCBIN "baserom.gbc",$14C000,$4000
+
 SECTION "bank54",DATA,BANK[$54]
+
 INCBIN "baserom.gbc",$150000,$4000
+
 SECTION "bank55",DATA,BANK[$55]
+
 INCBIN "baserom.gbc",$154000,$4000
+
 SECTION "bank56",DATA,BANK[$56]
+
 INCBIN "baserom.gbc",$158000,$4000
+
 SECTION "bank57",DATA,BANK[$57]
+
 INCBIN "baserom.gbc",$15C000,$4000
+
 SECTION "bank58",DATA,BANK[$58]
+
 INCBIN "baserom.gbc",$160000,$4000
+
 SECTION "bank59",DATA,BANK[$59]
+
 INCBIN "baserom.gbc",$164000,$4000
+
 SECTION "bank5A",DATA,BANK[$5A]
+
 INCBIN "baserom.gbc",$168000,$4000
+
 SECTION "bank5B",DATA,BANK[$5B]
+
 INCBIN "baserom.gbc",$16C000,$4000
+
 SECTION "bank5C",DATA,BANK[$5C]
+
 INCBIN "baserom.gbc",$170000,$4000
+
 SECTION "bank5D",DATA,BANK[$5D]
+
 INCBIN "baserom.gbc",$174000,$4000
+
 SECTION "bank5E",DATA,BANK[$5E]
+
 INCBIN "baserom.gbc",$178000,$4000
+
 SECTION "bank5F",DATA,BANK[$5F]
+
 INCBIN "baserom.gbc",$17C000,$4000
+
 SECTION "bank60",DATA,BANK[$60]
+
 INCBIN "baserom.gbc",$180000,$4000
+
 SECTION "bank61",DATA,BANK[$61]
+
 INCBIN "baserom.gbc",$184000,$4000
+
 SECTION "bank62",DATA,BANK[$62]
+
 INCBIN "baserom.gbc",$188000,$4000
+
 SECTION "bank63",DATA,BANK[$63]
+
 INCBIN "baserom.gbc",$18C000,$4000
+
 SECTION "bank64",DATA,BANK[$64]
+
 INCBIN "baserom.gbc",$190000,$4000
+
 SECTION "bank65",DATA,BANK[$65]
+
 INCBIN "baserom.gbc",$194000,$4000
+
 SECTION "bank66",DATA,BANK[$66]
+
 INCBIN "baserom.gbc",$198000,$4000
+
 SECTION "bank67",DATA,BANK[$67]
+
 INCBIN "baserom.gbc",$19C000,$4000
+
 SECTION "bank68",DATA,BANK[$68]
+
 INCBIN "baserom.gbc",$1A0000,$4000
+
 SECTION "bank69",DATA,BANK[$69]
+
 INCBIN "baserom.gbc",$1A4000,$4000
+
 SECTION "bank6A",DATA,BANK[$6A]
+
 INCBIN "baserom.gbc",$1A8000,$4000
+
 SECTION "bank6B",DATA,BANK[$6B]
+
 INCBIN "baserom.gbc",$1AC000,$4000
+
 SECTION "bank6C",DATA,BANK[$6C]
+
 INCBIN "baserom.gbc",$1B0000,$4000
+
 SECTION "bank6D",DATA,BANK[$6D]
+
 INCBIN "baserom.gbc",$1B4000,$4000
+
 SECTION "bank6E",DATA,BANK[$6E]
+
 INCBIN "baserom.gbc",$1B8000,$4000
+
 SECTION "bank6F",DATA,BANK[$6F]
+
 INCBIN "baserom.gbc",$1BC000,$4000
+
 SECTION "bank70",DATA,BANK[$70]
+
 INCBIN "baserom.gbc",$1C0000,$4000
+
 SECTION "bank71",DATA,BANK[$71]
+
 INCBIN "baserom.gbc",$1C4000,$4000
+
 SECTION "bank72",DATA,BANK[$72]
 
 	db "MASTER BALL@"
@@ -5410,29 +5625,55 @@ INCBIN "baserom.gbc",$1CA896,$1CAA43-$1CA896
 	db "SPECIAL@"
 
 INCBIN "baserom.gbc",$1CAEA1,$40
+
 SECTION "bank73",DATA,BANK[$73]
+
 INCBIN "baserom.gbc",$1CC000,$4000
+
 SECTION "bank74",DATA,BANK[$74]
+
 INCBIN "baserom.gbc",$1D0000,$4000
+
 SECTION "bank75",DATA,BANK[$75]
+
 INCBIN "baserom.gbc",$1D4000,$4000
+
 SECTION "bank76",DATA,BANK[$76]
+
 INCBIN "baserom.gbc",$1D8000,$4000
+
 SECTION "bank77",DATA,BANK[$77]
+
 INCBIN "baserom.gbc",$1DC000,$4000
+
 SECTION "bank78",DATA,BANK[$78]
+
 INCBIN "baserom.gbc",$1E0000,$4000
+
 SECTION "bank79",DATA,BANK[$79]
+
 INCBIN "baserom.gbc",$1E4000,$4000
+
 SECTION "bank7A",DATA,BANK[$7A]
+
 INCBIN "baserom.gbc",$1E8000,$4000
+
 SECTION "bank7B",DATA,BANK[$7B]
+
 INCBIN "baserom.gbc",$1EC000,$4000
+
 SECTION "bank7C",DATA,BANK[$7C]
+
 INCBIN "baserom.gbc",$1F0000,$4000
+
 SECTION "bank7D",DATA,BANK[$7D]
+
 INCBIN "baserom.gbc",$1F4000,$4000
+
 SECTION "bank7E",DATA,BANK[$7E]
+
 INCBIN "baserom.gbc",$1F8000,$4000
+
 SECTION "bank7F",DATA,BANK[$7F]
+
 INCBIN "baserom.gbc",$1FC000,$4000
