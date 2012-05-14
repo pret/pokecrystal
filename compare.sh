d3ecd6d10c61775e2ad384d37f0bca45c4b0c6fa
Bryan Bishop <kanzure@gmail.com>
2012-05-14 11:05:22 -0500

    a simple script to compare the baserom and generated rom

---
+++ compare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Compares baserom.gbc and pokecrystal.gbc
+
+# create baserom.txt if necessary
+if [ ! -f baserom.txt ]; then
+    hexdump -C baserom.gbc >> baserom.txt
+fi
+
+hexdump -C pokecrystal.gbc >> pokecrystal.txt
+
+diff baserom.txt pokecrystal.txt | less
