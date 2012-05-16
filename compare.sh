e27cdc43b2995e63e0d7aff5c252f1de6e799986
Bryan Bishop <kanzure@gmail.com>
2012-05-16 13:49:50 -0500

    make compare.sh overwrite the previous hexdump

--- compare.sh
+++ compare.sh
@@ -3,9 +3,9 @@
 
 # create baserom.txt if necessary
 if [ ! -f baserom.txt ]; then
-    hexdump -C baserom.gbc >> baserom.txt
+    hexdump -C baserom.gbc > baserom.txt
 fi
 
-hexdump -C pokecrystal.gbc >> pokecrystal.txt
+hexdump -C pokecrystal.gbc > pokecrystal.txt
 
 diff baserom.txt pokecrystal.txt | less
