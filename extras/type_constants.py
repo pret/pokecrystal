9a29def2503b00a971c7384e2071877aacf64eea
Bryan Bishop <kanzure@gmail.com>
2013-01-27 17:46:48 -0600

    basic type_constants data

---
+++ extras/type_constants.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+type_constants = {
+    "NORMAL": 0x00,
+    "FIGHTING": 0x01,
+    "FLYING": 0x02,
+    "POISON": 0x03,
+    "GROUND": 0x04,
+    "ROCK": 0x05,
+    "BUG": 0x07,
+    "GHOST": 0x08,
+    "STEEL": 0x09,
+    "CURSE_T": 0x13,
+    "FIRE": 0x14,
+    "WATER": 0x15,
+    "GRASS": 0x16,
+    "ELECTRIC": 0x17,
+    "PSYCHIC": 0x18,
+    "ICE": 0x19,
+    "DRAGON": 0x1A,
+    "DARK": 0x1B,
+}
