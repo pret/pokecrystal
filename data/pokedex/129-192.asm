70fd04dc24d4a33ddc7414591daa0763f22ba8c5
yenatch <yenatch@gmail.com>
2013-02-05 00:35:16 -0500

    Split pokedex entries off into files

---
+++ data/pokedex/129-192.asm
@@ -0,0 +1,511 @@
+MagikarpPokedexEntry: ; 0x1cc000
+	db "FISH@" ; species name
+	dw 211, 220; height, width
+	
+	db "This weak and", $4e, "pathetic #MON", $4e, "gets easily pushed@"
+	db "along rivers when", $4e, "there are strong", $4e, "currents.@"
+; 0x1cc065
+
+GyaradosPokedexEntry: ; 0x1cc065
+	db "ATROCIOUS@" ; species name
+	dw 2104, 5180; height, width
+	
+	db "It appears when-", $4e, "ever there is", $4e, "world conflict,@"
+	db "burning down any", $4e, "place it travels", $4e, "through.@"
+; 0x1cc0cd
+
+LaprasPokedexEntry: ; 0x1cc0cd
+	db "TRANSPORT@" ; species name
+	dw 802, 4850; height, width
+	
+	db "This gentle", $4e, "#MON loves to", $4e, "give people rides@"
+	db "and provides a ve-", $4e, "ry comfortable way", $4e, "to get around.@"
+; 0x1cc13c
+
+DittoPokedexEntry: ; 0x1cc13c
+	db "TRANSFORM@" ; species name
+	dw 100, 90; height, width
+	
+	db "When it encount-", $4e, "ers another DITTO,", $4e, "it will move@"
+	db "faster than normal", $4e, "to duplicate that", $4e, "opponent exactly.@"
+; 0x1cc1b2
+
+EeveePokedexEntry: ; 0x1cc1b2
+	db "EVOLUTION@" ; species name
+	dw 100, 140; height, width
+	
+	db "Its ability to", $4e, "evolve into many", $4e, "forms allows it to@"
+	db "adapt smoothly", $4e, "and perfectly to", $4e, "any environment.@"
+; 0x1cc224
+
+VaporeonPokedexEntry: ; 0x1cc224
+	db "BUBBLE JET@" ; species name
+	dw 303, 640; height, width
+	
+	db "As it uses the", $4e, "fins on the tip", $4e, "of its tail to@"
+	db "swim, it blends", $4e, "with the water", $4e, "perfectly.@"
+; 0x1cc28b
+
+JolteonPokedexEntry: ; 0x1cc28b
+	db "LIGHTNING@" ; species name
+	dw 207, 540; height, width
+	
+	db "The negatively", $4e, "charged ions", $4e, "generated in its@"
+	db "fur create a", $4e, "constant sparking", $4e, "noise.@"
+; 0x1cc2ec
+
+FlareonPokedexEntry: ; 0x1cc2ec
+	db "FLAME@" ; species name
+	dw 211, 550; height, width
+	
+	db "Once it has stored", $4e, "up enough heat,", $4e, "this #MON's@"
+	db "body temperature", $4e, "can reach up to", $4e, "1700 degrees.@"
+; 0x1cc353
+
+PorygonPokedexEntry: ; 0x1cc353
+	db "VIRTUAL@" ; species name
+	dw 207, 800; height, width
+	
+	db "An artificial", $4e, "#MON created", $4e, "due to extensive@"
+	db "research, it can", $4e, "perform only what", $4e, "is in its program.@"
+; 0x1cc3c1
+
+OmanytePokedexEntry: ; 0x1cc3c1
+	db "SPIRAL@" ; species name
+	dw 104, 170; height, width
+	
+	db "In prehistoric", $4e, "times, it swam on", $4e, "the sea floor,@"
+	db "eating plankton.", $4e, "Its fossils are", $4e, "sometimes found.@"
+; 0x1cc42e
+
+OmastarPokedexEntry: ; 0x1cc42e
+	db "SPIRAL@" ; species name
+	dw 303, 770; height, width
+	
+	db "Its heavy shell", $4e, "allowed it to", $4e, "reach only nearby@"
+	db "food. This could", $4e, "be the reason it", $4e, "is extinct.@"
+; 0x1cc497
+
+KabutoPokedexEntry: ; 0x1cc497
+	db "SHELLFISH@" ; species name
+	dw 108, 250; height, width
+	
+	db "Three hundred", $4e, "million years ago,", $4e, "it hid on the sea@"
+	db "floor. It also has", $4e, "eyes on its back", $4e, "that glow.@"
+; 0x1cc507
+
+KabutopsPokedexEntry: ; 0x1cc507
+	db "SHELLFISH@" ; species name
+	dw 403, 890; height, width
+	
+	db "It was able to", $4e, "swim quickly thro-", $4e, "ugh the water by@"
+	db "compactly folding", $4e, "up its razor-sharp", $4e, "sickles.@"
+; 0x1cc576
+
+AerodactylPokedexEntry: ; 0x1cc576
+	db "FOSSIL@" ; species name
+	dw 511, 1300; height, width
+	
+	db "In prehistoric", $4e, "times, this", $4e, "#MON flew@"
+	db "freely and", $4e, "fearlessly through", $4e, "the skies.@"
+; 0x1cc5cf
+
+SnorlaxPokedexEntry: ; 0x1cc5cf
+	db "SLEEPING@" ; species name
+	dw 611, 10140; height, width
+	
+	db "This #MON's", $4e, "stomach is so", $4e, "strong, even@"
+	db "eating moldy or", $4e, "rotten food will", $4e, "not affect it.@"
+; 0x1cc632
+
+ArticunoPokedexEntry: ; 0x1cc632
+	db "FREEZE@" ; species name
+	dw 507, 1220; height, width
+	
+	db "Legendary bird", $4e, "#MON. As it", $4e, "flies through the@"
+	db "sky, it cools the", $4e, "air, causing snow", $4e, "to fall.@"
+; 0x1cc697
+
+ZapdosPokedexEntry: ; 0x1cc697
+	db "ELECTRIC@" ; species name
+	dw 503, 1160; height, width
+	
+	db "Legendary bird", $4e, "#MON. They say", $4e, "lightning caused@"
+	db "by the flapping of", $4e, "its wings causes", $4e, "summer storms.@"
+; 0x1cc706
+
+MoltresPokedexEntry: ; 0x1cc706
+	db "FLAME@" ; species name
+	dw 607, 1320; height, width
+	
+	db "Legendary bird", $4e, "#MON. It is", $4e, "said to migrate@"
+	db "from the south", $4e, "along with the", $4e, "spring.@"
+; 0x1cc761
+
+DratiniPokedexEntry: ; 0x1cc761
+	db "DRAGON@" ; species name
+	dw 511, 70; height, width
+	
+	db "It sheds many lay-", $4e, "ers of skin as it", $4e, "grows larger. Dur-@"
+	db "ing this process,", $4e, "it is protected by", $4e, "a rapid waterfall.@"
+; 0x1cc7dc
+
+DragonairPokedexEntry: ; 0x1cc7dc
+	db "DRAGON@" ; species name
+	dw 1301, 360; height, width
+	
+	db "It is called the", $4e, "divine #MON.", $4e, "When its entire@"
+	db "body brightens", $4e, "slightly, the", $4e, "weather changes.@"
+; 0x1cc843
+
+DragonitePokedexEntry: ; 0x1cc843
+	db "DRAGON@" ; species name
+	dw 703, 4630; height, width
+	
+	db "It is said that", $4e, "somewhere in the", $4e, "ocean lies an@"
+	db "island where these", $4e, "gather. Only they", $4e, "live there.@"
+; 0x1cc8ae
+
+MewtwoPokedexEntry: ; 0x1cc8ae
+	db "GENETIC@" ; species name
+	dw 607, 2690; height, width
+	
+	db "Said to rest qui-", $4e, "etly in an", $4e, "undiscovered cave,@"
+	db "this #MON was", $4e, "created solely for", $4e, "battling.@"
+; 0x1cc915
+
+MewPokedexEntry: ; 0x1cc915
+	db "NEW SPECIE@" ; species name
+	dw 104, 90; height, width
+	
+	db "Because it can", $4e, "learn any move,", $4e, "some people began@"
+	db "research to see if", $4e, "it is the ancestor", $4e, "of all #MON.@"
+; 0x1cc988
+
+ChikoritaPokedexEntry: ; 0x1cc988
+	db "LEAF@" ; species name
+	dw 211, 140; height, width
+	
+	db "It loves to bask", $4e, "in the sunlight.", $4e, "It uses the leaf@"
+	db "on its head to", $4e, "seek out warm", $4e, "places.@"
+; 0x1cc9e9
+
+BayleefPokedexEntry: ; 0x1cc9e9
+	db "LEAF@" ; species name
+	dw 311, 350; height, width
+	
+	db "The scent that", $4e, "wafts from the", $4e, "leaves on its neck@"
+	db "causes anyone who", $4e, "smells it to", $4e, "become energetic.@"
+; 0x1cca54
+
+MeganiumPokedexEntry: ; 0x1cca54
+	db "HERB@" ; species name
+	dw 511, 2220; height, width
+	
+	db "Anyone who stands", $4e, "beside it becomes", $4e, "refreshed, just as@"
+	db "if they were", $4e, "relaxing in a", $4e, "sunny forest.@"
+; 0x1ccabd
+
+CyndaquilPokedexEntry: ; 0x1ccabd
+	db "FIRE MOUSE@" ; species name
+	dw 108, 170; height, width
+	
+	db "The fire that", $4e, "spouts from its", $4e, "back burns hottest@"
+	db "when it is angry.", $4e, "The flaring flames", $4e, "intimidate foes.@"
+; 0x1ccb33
+
+QuilavaPokedexEntry: ; 0x1ccb33
+	db "VOLCANO@" ; species name
+	dw 211, 420; height, width
+	
+	db "Before battle, it", $4e, "turns its back on", $4e, "its opponent to@"
+	db "demonstrate how", $4e, "ferociously its", $4e, "fire blazes.@"
+; 0x1ccba0
+
+TyphlosionPokedexEntry: ; 0x1ccba0
+	db "VOLCANO@" ; species name
+	dw 507, 1750; height, width
+	
+	db "When heat from its", $4e, "body causes the", $4e, "air around it to@"
+	db "shimmer, this is a", $4e, "sign that it is", $4e, "ready to battle.@"
+; 0x1ccc14
+
+TotodilePokedexEntry: ; 0x1ccc14
+	db "BIG JAW@" ; species name
+	dw 200, 210; height, width
+	
+	db "This rough critter", $4e, "chomps at any", $4e, "moving object it@"
+	db "sees. Turning your", $4e, "back on it is not", $4e, "recommended.@"
+; 0x1ccc84
+
+CroconawPokedexEntry: ; 0x1ccc84
+	db "BIG JAW@" ; species name
+	dw 307, 550; height, width
+	
+	db "The tips of its", $4e, "fangs are slanted", $4e, "backward. Once@"
+	db "those fangs clamp", $4e, "down, the prey has", $4e, "no hope of escape.@"
+; 0x1cccf9
+
+FeraligatrPokedexEntry: ; 0x1cccf9
+	db "BIG JAW@" ; species name
+	dw 707, 1960; height, width
+	
+	db "Although it has a", $4e, "massive body, its", $4e, "powerful hind legs@"
+	db "enable it to move", $4e, "quickly, even on", $4e, "the ground.@"
+; 0x1ccd6b
+
+SentretPokedexEntry: ; 0x1ccd6b
+	db "SCOUT@" ; species name
+	dw 207, 130; height, width
+	
+	db "When acting as a", $4e, "lookout, it warns", $4e, "others of danger@"
+	db "by screeching and", $4e, "hitting the ground", $4e, "with its tail.@"
+; 0x1ccddd
+
+FurretPokedexEntry: ; 0x1ccddd
+	db "LONG BODY@" ; species name
+	dw 511, 720; height, width
+	
+	db "It lives in narrow", $4e, "burrows that fit", $4e, "its slim body. The@"
+	db "deeper the nests", $4e, "go, the more maze-", $4e, "like they become.@"
+; 0x1cce58
+
+HoothootPokedexEntry: ; 0x1cce58
+	db "OWL@" ; species name
+	dw 204, 470; height, width
+	
+	db "It begins to hoot", $4e, "at the same time", $4e, "every day. Some@"
+	db "trainers use them", $4e, "in place of", $4e, "clocks.@"
+; 0x1cceb9
+
+NoctowlPokedexEntry: ; 0x1cceb9
+	db "OWL@" ; species name
+	dw 503, 900; height, width
+	
+	db "Its extremely soft", $4e, "feathers make no", $4e, "sound in flight.@"
+	db "It silently sneaks", $4e, "up on prey without", $4e, "being detected.@"
+; 0x1ccf2c
+
+LedybaPokedexEntry: ; 0x1ccf2c
+	db "FIVE STAR@" ; species name
+	dw 303, 240; height, width
+	
+	db "It is timid and", $4e, "clusters together", $4e, "with others. The@"
+	db "fluid secreted by", $4e, "its feet indicates", $4e, "its location.@"
+; 0x1ccfa0
+
+LedianPokedexEntry: ; 0x1ccfa0
+	db "FIVE STAR@" ; species name
+	dw 407, 780; height, width
+	
+	db "In the daytime", $4e, "when it gets warm,", $4e, "it curls up inside@"
+	db "a big leaf and", $4e, "drifts off into", $4e, "a deep slumber.@"
+; 0x1cd012
+
+SpinarakPokedexEntry: ; 0x1cd012
+	db "STRINGSPIT@" ; species name
+	dw 108, 190; height, width
+	
+	db "If prey becomes", $4e, "ensnared in its", $4e, "nest of spun@"
+	db "string, it waits", $4e, "motionlessly until", $4e, "it becomes dark.@"
+; 0x1cd083
+
+AriadosPokedexEntry: ; 0x1cd083
+	db "LONG LEG@" ; species name
+	dw 307, 740; height, width
+	
+	db "Rather than mak-", $4e, "ing a nest in one", $4e, "specific spot, it@"
+	db "wanders in search", $4e, "of food after", $4e, "darkness falls.@"
+; 0x1cd0f5
+
+CrobatPokedexEntry: ; 0x1cd0f5
+	db "BAT@" ; species name
+	dw 511, 1650; height, width
+	
+	db "As a result of its", $4e, "pursuit of faster,", $4e, "yet more silent@"
+	db "flight, a new set", $4e, "of wings grew on", $4e, "its hind legs.@"
+; 0x1cd165
+
+ChinchouPokedexEntry: ; 0x1cd165
+	db "ANGLER@" ; species name
+	dw 108, 260; height, width
+	
+	db "Its antennae, whi-", $4e, "ch evolved from a", $4e, "fin, have both po-@"
+	db "sitive and neg-", $4e, "ative charges flo-", $4e, "wing through them.@"
+; 0x1cd1de
+
+LanturnPokedexEntry: ; 0x1cd1de
+	db "LIGHT@" ; species name
+	dw 311, 500; height, width
+	
+	db "This #MON uses", $4e, "the bright part of", $4e, "its body, which@"
+	db "changed from a", $4e, "dorsal fin, to", $4e, "lure prey.@"
+; 0x1cd243
+
+PichuPokedexEntry: ; 0x1cd243
+	db "TINY MOUSE@" ; species name
+	dw 100, 40; height, width
+	
+	db "It is unskilled at", $4e, "storing electric", $4e, "power. Any kind of@"
+	db "shock causes it to", $4e, "discharge energy", $4e, "spontaneously.@"
+; 0x1cd2bc
+
+CleffaPokedexEntry: ; 0x1cd2bc
+	db "STARSHAPE@" ; species name
+	dw 100, 70; height, width
+	
+	db "If the impact site", $4e, "of a meteorite is", $4e, "found, this@"
+	db "#MON is certain", $4e, "to be within the", $4e, "immediate area.@"
+; 0x1cd32c
+
+IgglybuffPokedexEntry: ; 0x1cd32c
+	db "BALLOON@" ; species name
+	dw 100, 20; height, width
+	
+	db "Instead of walking", $4e, "with its short", $4e, "legs, it moves@"
+	db "around by bouncing", $4e, "on its soft,", $4e, "tender body.@"
+; 0x1cd396
+
+TogepiPokedexEntry: ; 0x1cd396
+	db "SPIKE BALL@" ; species name
+	dw 100, 30; height, width
+	
+	db "It is considered", $4e, "to be a symbol of", $4e, "good luck. Its@"
+	db "shell is said to", $4e, "be filled with", $4e, "happiness.@"
+; 0x1cd402
+
+TogeticPokedexEntry: ; 0x1cd402
+	db "HAPPINESS@" ; species name
+	dw 200, 70; height, width
+	
+	db "Although it does", $4e, "not flap its wings", $4e, "very much, it can@"
+	db "stay up in the air", $4e, "as it tags along", $4e, "after its trainer.@"
+; 0x1cd47d
+
+NatuPokedexEntry: ; 0x1cd47d
+	db "LITTLE BIRD@" ; species name
+	dw 8, 40; height, width
+	
+	db "It is extremely", $4e, "good at climbing", $4e, "tree trunks and@"
+	db "likes to eat the", $4e, "new sprouts on", $4e, "the trees.@"
+; 0x1cd4e9
+
+XatuPokedexEntry: ; 0x1cd4e9
+	db "MYSTIC@" ; species name
+	dw 411, 330; height, width
+	
+	db "Once it begins to", $4e, "meditate at sun-", $4e, "rise, the entire@"
+	db "day will pass", $4e, "before it will", $4e, "move again.@"
+; 0x1cd551
+
+MareepPokedexEntry: ; 0x1cd551
+	db "WOOL@" ; species name
+	dw 200, 170; height, width
+	
+	db "It stores lots of", $4e, "air in its soft", $4e, "fur, allowing it@"
+	db "to stay cool in", $4e, "summer and warm", $4e, "in winter.@"
+; 0x1cd5b8
+
+FlaaffyPokedexEntry: ; 0x1cd5b8
+	db "WOOL@" ; species name
+	dw 207, 290; height, width
+	
+	db "Because of its", $4e, "rubbery, electric-", $4e, "ity-resistant@"
+	db "skin, it can store", $4e, "lots of electric-", $4e, "ity in its fur.@"
+; 0x1cd626
+
+AmpharosPokedexEntry: ; 0x1cd626
+	db "LIGHT@" ; species name
+	dw 407, 1360; height, width
+	
+	db "When it gets dark,", $4e, "the light from its", $4e, "bright, shiny tail@"
+	db "can be seen from", $4e, "far away on the", $4e, "ocean's surface.@"
+; 0x1cd69a
+
+BellossomPokedexEntry: ; 0x1cd69a
+	db "FLOWER@" ; species name
+	dw 104, 130; height, width
+	
+	db "When these dance", $4e, "together, their", $4e, "petals rub against@"
+	db "each other,", $4e, "making pretty,", $4e, "relaxing sounds.@"
+; 0x1cd705
+
+MarillPokedexEntry: ; 0x1cd705
+	db "AQUAMOUSE@" ; species name
+	dw 104, 190; height, width
+	
+	db "The fur on its", $4e, "body naturally", $4e, "repels water. It@"
+	db "can stay dry, even", $4e, "when it plays in", $4e, "the water.@"
+; 0x1cd771
+
+AzumarillPokedexEntry: ; 0x1cd771
+	db "AQUARABBIT@" ; species name
+	dw 207, 630; height, width
+	
+	db "The bubble-like", $4e, "pattern on its", $4e, "stomach helps it@"
+	db "camouflage itself", $4e, "when it's in the", $4e, "water.@"
+; 0x1cd7d9
+
+SudowoodoPokedexEntry: ; 0x1cd7d9
+	db "IMITATION@" ; species name
+	dw 311, 840; height, width
+	
+	db "If a tree branch", $4e, "shakes when there", $4e, "is no wind, it's a@"
+	db "SUDOWOODO, not a", $4e, "tree. It hides", $4e, "from the rain.@"
+; 0x1cd84b
+
+PolitoedPokedexEntry: ; 0x1cd84b
+	db "FROG@" ; species name
+	dw 307, 750; height, width
+	
+	db "When it expands", $4e, "its throat to", $4e, "croak out a tune,@"
+	db "nearby POLIWAG and", $4e, "POLIWHIRL gather", $4e, "immediately.@"
+; 0x1cd8b5
+
+HoppipPokedexEntry: ; 0x1cd8b5
+	db "COTTONWEED@" ; species name
+	dw 104, 10; height, width
+	
+	db "It can be carried", $4e, "away on even the", $4e, "gentlest breeze.@"
+	db "It may even float", $4e, "all the way to the", $4e, "next town.@"
+; 0x1cd928
+
+SkiploomPokedexEntry: ; 0x1cd928
+	db "COTTONWEED@" ; species name
+	dw 200, 20; height, width
+	
+	db "As soon as it", $4e, "rains, it closes", $4e, "its flower and@"
+	db "hides in the shade", $4e, "of a tree to avoid", $4e, "getting wet.@"
+; 0x1cd998
+
+JumpluffPokedexEntry: ; 0x1cd998
+	db "COTTONWEED@" ; species name
+	dw 207, 70; height, width
+	
+	db "Even in the fierc-", $4e, "est wind, it can", $4e, "control its fluff@"
+	db "to make its way to", $4e, "any place in the", $4e, "world it wants.@"
+; 0x1cda11
+
+AipomPokedexEntry: ; 0x1cda11
+	db "LONG TAIL@" ; species name
+	dw 207, 250; height, width
+	
+	db "It uses its tail", $4e, "to hang on to tree", $4e, "branches. It uses@"
+	db "its momentum to", $4e, "swing from one", $4e, "branch to another.@"
+; 0x1cda87
+
+SunkernPokedexEntry: ; 0x1cda87
+	db "SEED@" ; species name
+	dw 100, 40; height, width
+	
+	db "It is very weak.", $4e, "Its only means of", $4e, "defense is to@"
+	db "shake its leaves", $4e, "desperately at its", $4e, "attacker.@"
+; 0x1cdaef
+
+SunfloraPokedexEntry: ; 0x1cdaef
+	db "SUN@" ; species name
+	dw 207, 190; height, width
+	
+	db "As the hot season", $4e, "approaches, the", $4e, "petals on this@"
+	db "#MON's face", $4e, "become more vivid", $4e, "and lively.@"
+; 0x1cdb51
