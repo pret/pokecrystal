b74e63a20dcdb0cf376977426a9dcbe64586273a
yenatch <yenatch@gmail.com>
2013-09-03 17:36:55 -0400

    split out delay functions

--- main.asm
+++ main.asm
@@ -62,28 +62,7 @@ INCLUDE "engine/init.asm"
 INCLUDE "engine/vblank.asm"
 
 
-DelayFrame: ; 45a
-; Wait for one frame
-	ld a, 1
-	ld [VBlankOccurred], a
-
-; Wait for the next VBlank, halting to conserve battery
-.halt
-	halt ; rgbasm adds a nop after this instruction by default
-	ld a, [VBlankOccurred]
-	and a
-	jr nz, .halt
-	ret
-; 468
-
-
-DelayFrames: ; 468
-; Wait c frames
-	call DelayFrame
-	dec c
-	jr nz, DelayFrames
-	ret
-; 46f
+INCLUDE "engine/delay.asm"
 
 
 RTC: ; 46f
