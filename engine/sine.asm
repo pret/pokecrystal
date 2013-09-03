23bb05851b0f71ca7cf9277d46f09e799986350d
yenatch <yenatch@gmail.com>
2013-09-03 18:40:12 -0400

    split Sine/Cosine into engine/sine.asm

---
+++ engine/sine.asm
@@ -0,0 +1,22 @@
+Cosine: ; 1b0f
+; Return d * cos(a) in hl
+	add $10 ; 90 degrees
+
+Sine: ; 1b11
+; Return d * sin(a) in hl
+; a is a signed 6-bit value.
+
+	ld e, a
+
+	ld a, [hROMBank]
+	push af
+	ld a, BANK(_Sine)
+	rst Bankswitch
+
+	call _Sine
+
+	pop af
+	rst Bankswitch
+	ret
+; 1b1e
+
--- main.asm
+++ main.asm
@@ -29,30 +29,7 @@ INCLUDE "engine/copy.asm"
 INCLUDE "engine/text.asm"
 INCLUDE "engine/video.asm"
 INCLUDE "engine/map_objects.asm"
-
-
-Cosine: ; 1b0f
-; Return d * cos(a) in hl
-	add $10 ; 90 degrees
-
-Sine: ; 1b11
-; Return d * sin(a) in hl
-; a is a signed 6-bit value.
-
-	ld e, a
-
-	ld a, [hROMBank]
-	push af
-	ld a, BANK(_Sine)
-	rst Bankswitch
-
-	call _Sine
-
-	pop af
-	rst Bankswitch
-	ret
-; 1b1e
-
+INCLUDE "engine/sine.asm"
 
 Function1b1e: ; 1b1e
 	ld [$d003], a
