0ce5c4fe9327180aed48275d61c56e21eee473f7
yenatch <yenatch@gmail.com>
2013-09-15 18:56:25 -0400

    split the mobile adapter library into lib/mobile/

--- main.asm
+++ main.asm
@@ -79682,20 +79682,8 @@ TitleScreenPalettes:
 
 SECTION "bank44",ROMX,BANK[$44]
 
-INCBIN "baserom.gbc", $110000, $110fad - $110000
+INCLUDE "lib/mobile/main.asm"
 
-URIPrefix: ; 0x110fad
-	ascii "http://"
-HTTPDownloadURL: ; 0x110fb4
-	ascii "gameboy.datacenter.ne.jp/cgb/download"
-HTTPUploadURL: ; 0x110fd9
-	ascii "gameboy.datacenter.ne.jp/cgb/upload"
-HTTPUtilityURL: ; 0x110ffc
-	ascii "gameboy.datacenter.ne.jp/cgb/utility"
-HTTPRankingURL: ; 0x111020
-	ascii "gameboy.datacenter.ne.jp/cgb/ranking"
-
-INCBIN "baserom.gbc", $111044, $113f84 - $111044
 
 SECTION "bank45",ROMX,BANK[$45]
 
