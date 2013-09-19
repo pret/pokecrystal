f6a5d7b2f2bce987ae7d5e291e80e830516c2965
yenatch <yenatch@gmail.com>
2013-09-18 21:54:31 -0400

    split pokedex flag handling into home/pokedex_flags.asm

--- main.asm
+++ main.asm
@@ -1281,45 +1281,7 @@ GetWeekday: ; 3376
 ; 3380
 
 
-SetSeenAndCaughtMon: ; 3380
-	push af
-	ld c, a
-	ld hl, PokedexCaught
-	ld b, SET_FLAG
-	call PokedexFlagAction
-	pop af
-	; fallthrough
-; 338b
-
-SetSeenMon: ; 338b
-	ld c, a
-	ld hl, PokedexSeen
-	ld b, SET_FLAG
-	jr PokedexFlagAction
-; 3393
-
-CheckCaughtMon: ; 3393
-	ld c, a
-	ld hl, PokedexCaught
-	ld b, CHECK_FLAG
-	jr PokedexFlagAction
-; 339b
-
-CheckSeenMon: ; 339b
-	ld c, a
-	ld hl, PokedexSeen
-	ld b, CHECK_FLAG
-	; fallthrough
-; 33a1
-
-PokedexFlagAction: ; 33a1
-	ld d, 0
-	ld a, PREDEF_FLAG
-	call Predef
-	ld a, c
-	and a
-	ret
-; 33ab
+INCLUDE "home/pokedex_flags.asm"
 
 
 NamesPointerTable: ; 33ab
