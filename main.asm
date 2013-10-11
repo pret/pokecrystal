8b8de4052548a8a1dd5da72cc2e0f3a536847b2b
yenatch <yenatch@gmail.com>
2013-10-11 01:36:11 -0400

    roofs for each map group

--- main.asm
+++ main.asm
@@ -27882,22 +27882,54 @@ SECTION "bank7",ROMX,BANK[$7]
 Function1c000: ; 1c000
 	ld a, [MapGroup]
 	ld e, a
-	ld d, $0
-	ld hl, $4021
+	ld d, 0
+	ld hl, MapGroupRoofs
 	add hl, de
 	ld a, [hl]
 	cp $ff
 	ret z
-	ld hl, $403c
-	ld bc, $0090
+	ld hl, Roofs
+	ld bc, $90
 	call AddNTimes
 	ld de, $90a0
-	ld bc, $0090
+	ld bc, $90
 	call CopyBytes
 	ret
 ; 1c021
 
-INCBIN "baserom.gbc", $1c021, $1c30c - $1c021
+MapGroupRoofs: ; 1c021i
+	db -1 ; group 1
+	db  3 ; group 2
+	db  2 ; group 3
+	db -1 ; group 4
+	db  1 ; group 5
+	db  2 ; group 6
+	db -1 ; group 7
+	db -1 ; group 8
+	db  2 ; group 9
+	db  2 ; group 10
+	db  1 ; group 11
+	db  4 ; group 12
+	db -1 ; group 13
+	db -1 ; group 14
+	db -1 ; group 15
+	db -1 ; group 16
+	db -1 ; group 17
+	db -1 ; group 18
+	db -1 ; group 19
+	db  0 ; group 20
+	db -1 ; group 21
+	db -1 ; group 22
+	db  3 ; group 23
+	db -1 ; group 24
+	db  0 ; group 25
+	db -1 ; group 26
+	db  0 ; group 27
+; 1c03c
+
+Roofs: ; 1c03c
+INCBIN "baserom.gbc", $1c03c, $1c30c - $1c03c
+; 1c30c
 
 INCLUDE "tilesets/data_2.asm"
 
