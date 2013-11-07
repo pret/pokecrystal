fa3145dd4419d069a0c39f7a75f7e47ded34e401
yenatch <yenatch@gmail.com>
2013-11-07 01:11:42 -0500

    rework note macro

--- macros.asm
+++ macros.asm
@@ -51,9 +51,26 @@ RGB: MACRO
 	dw ((\3 << 10) | (\2 << 5) | (\1))
 	ENDM
 
+
 note: MACRO
-	db \1
+	db \1 << 4 + \2
 	ENDM
+
+; pitch
+__ EQU 0
+C_ EQU 1
+C# EQU 2
+D_ EQU 3
+D# EQU 4
+E_ EQU 5
+F_ EQU 6
+F# EQU 7
+G_ EQU 8
+G# EQU 9
+A_ EQU 10
+A# EQU 11
+B_ EQU 12
+
 
 ; It's better to use *coord than FuncCoord.
 FuncCoord: MACRO
