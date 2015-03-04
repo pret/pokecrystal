636b336a509db4e38be2319289f53637b4e90d48
yenatch <yenatch@gmail.com>
2015-03-03 20:32:53 -0500

    Redump the overworld graphics; grouped properly this time.

--- gfx/overworld/sprites_2.asm
+++ gfx/overworld/sprites_2.asm
@@ -1,430 +1,58 @@
-PokefanMSpriteGFX: ; c4000
-INCBIN "gfx/overworld/256.2bpp"
-INCBIN "gfx/overworld/257.2bpp"
-INCBIN "gfx/overworld/258.2bpp"
-INCBIN "gfx/overworld/259.2bpp"
-INCBIN "gfx/overworld/260.2bpp"
-INCBIN "gfx/overworld/261.2bpp"
-; c4180
-
-PokefanFSpriteGFX: ; c4180
-INCBIN "gfx/overworld/262.2bpp"
-INCBIN "gfx/overworld/263.2bpp"
-INCBIN "gfx/overworld/264.2bpp"
-INCBIN "gfx/overworld/265.2bpp"
-INCBIN "gfx/overworld/266.2bpp"
-INCBIN "gfx/overworld/267.2bpp"
-; c4300
-
-GrampsSpriteGFX: ; c4300
-INCBIN "gfx/overworld/268.2bpp"
-INCBIN "gfx/overworld/269.2bpp"
-INCBIN "gfx/overworld/270.2bpp"
-INCBIN "gfx/overworld/271.2bpp"
-INCBIN "gfx/overworld/272.2bpp"
-INCBIN "gfx/overworld/273.2bpp"
-; c4480
-
-GrannySpriteGFX: ; c4480
-INCBIN "gfx/overworld/274.2bpp"
-INCBIN "gfx/overworld/275.2bpp"
-INCBIN "gfx/overworld/276.2bpp"
-INCBIN "gfx/overworld/277.2bpp"
-INCBIN "gfx/overworld/278.2bpp"
-INCBIN "gfx/overworld/279.2bpp"
-; c4600
-
-SwimmerGuySpriteGFX: ; c4600
-INCBIN "gfx/overworld/280.2bpp"
-INCBIN "gfx/overworld/281.2bpp"
-INCBIN "gfx/overworld/282.2bpp"
-INCBIN "gfx/overworld/283.2bpp"
-INCBIN "gfx/overworld/284.2bpp"
-INCBIN "gfx/overworld/285.2bpp"
-; c4780
-
-SwimmerGirlSpriteGFX: ; c4780
-INCBIN "gfx/overworld/286.2bpp"
-INCBIN "gfx/overworld/287.2bpp"
-INCBIN "gfx/overworld/288.2bpp"
-INCBIN "gfx/overworld/289.2bpp"
-INCBIN "gfx/overworld/290.2bpp"
-INCBIN "gfx/overworld/291.2bpp"
-; c4900
-
-BigSnorlaxSpriteGFX: ; c4900
-INCBIN "gfx/overworld/292.2bpp"
-INCBIN "gfx/overworld/293.2bpp"
-; c4980
-
-SurfingPikachuSpriteGFX: ; c4980
-INCBIN "gfx/overworld/294.2bpp"
-INCBIN "gfx/overworld/295.2bpp"
-INCBIN "gfx/overworld/296.2bpp"
-INCBIN "gfx/overworld/297.2bpp"
-INCBIN "gfx/overworld/298.2bpp"
-INCBIN "gfx/overworld/299.2bpp"
-; c4b00
-
-RocketSpriteGFX: ; c4b00
-INCBIN "gfx/overworld/300.2bpp"
-INCBIN "gfx/overworld/301.2bpp"
-INCBIN "gfx/overworld/302.2bpp"
-INCBIN "gfx/overworld/303.2bpp"
-INCBIN "gfx/overworld/304.2bpp"
-INCBIN "gfx/overworld/305.2bpp"
-; c4c80
-
-RocketGirlSpriteGFX: ; c4c80
-INCBIN "gfx/overworld/306.2bpp"
-INCBIN "gfx/overworld/307.2bpp"
-INCBIN "gfx/overworld/308.2bpp"
-INCBIN "gfx/overworld/309.2bpp"
-INCBIN "gfx/overworld/310.2bpp"
-INCBIN "gfx/overworld/311.2bpp"
-; c4e00
-
-NurseSpriteGFX: ; c4e00
-INCBIN "gfx/overworld/312.2bpp"
-INCBIN "gfx/overworld/313.2bpp"
-INCBIN "gfx/overworld/314.2bpp"
-; c4ec0
-
-LinkReceptionistSpriteGFX: ; c4ec0
-INCBIN "gfx/overworld/315.2bpp"
-INCBIN "gfx/overworld/316.2bpp"
-INCBIN "gfx/overworld/317.2bpp"
-INCBIN "gfx/overworld/318.2bpp"
-INCBIN "gfx/overworld/319.2bpp"
-INCBIN "gfx/overworld/320.2bpp"
-; c5040
-
-ClerkSpriteGFX: ; c5040
-INCBIN "gfx/overworld/321.2bpp"
-INCBIN "gfx/overworld/322.2bpp"
-INCBIN "gfx/overworld/323.2bpp"
-INCBIN "gfx/overworld/324.2bpp"
-INCBIN "gfx/overworld/325.2bpp"
-INCBIN "gfx/overworld/326.2bpp"
-; c51c0
-
-FisherSpriteGFX: ; c51c0
-INCBIN "gfx/overworld/327.2bpp"
-INCBIN "gfx/overworld/328.2bpp"
-INCBIN "gfx/overworld/329.2bpp"
-INCBIN "gfx/overworld/330.2bpp"
-INCBIN "gfx/overworld/331.2bpp"
-INCBIN "gfx/overworld/332.2bpp"
-; c5340
-
-FishingGuruSpriteGFX: ; c5340
-INCBIN "gfx/overworld/333.2bpp"
-INCBIN "gfx/overworld/334.2bpp"
-INCBIN "gfx/overworld/335.2bpp"
-INCBIN "gfx/overworld/336.2bpp"
-INCBIN "gfx/overworld/337.2bpp"
-INCBIN "gfx/overworld/338.2bpp"
-; c54c0
-
-ScientistSpriteGFX: ; c54c0
-INCBIN "gfx/overworld/339.2bpp"
-INCBIN "gfx/overworld/340.2bpp"
-INCBIN "gfx/overworld/341.2bpp"
-INCBIN "gfx/overworld/342.2bpp"
-INCBIN "gfx/overworld/343.2bpp"
-INCBIN "gfx/overworld/344.2bpp"
-; c5640
-
-KimonoGirlSpriteGFX: ; c5640
-INCBIN "gfx/overworld/345.2bpp"
-INCBIN "gfx/overworld/346.2bpp"
-INCBIN "gfx/overworld/347.2bpp"
-INCBIN "gfx/overworld/348.2bpp"
-INCBIN "gfx/overworld/349.2bpp"
-INCBIN "gfx/overworld/350.2bpp"
-; c57c0
-
-SageSpriteGFX: ; c57c0
-INCBIN "gfx/overworld/351.2bpp"
-INCBIN "gfx/overworld/352.2bpp"
-INCBIN "gfx/overworld/353.2bpp"
-INCBIN "gfx/overworld/354.2bpp"
-INCBIN "gfx/overworld/355.2bpp"
-INCBIN "gfx/overworld/356.2bpp"
-; c5940
-
-UnusedGuySpriteGFX: ; c5940
-INCBIN "gfx/overworld/357.2bpp"
-INCBIN "gfx/overworld/358.2bpp"
-INCBIN "gfx/overworld/359.2bpp"
-INCBIN "gfx/overworld/360.2bpp"
-INCBIN "gfx/overworld/361.2bpp"
-INCBIN "gfx/overworld/362.2bpp"
-; c5ac0
-
-GentlemanSpriteGFX: ; c5ac0
-INCBIN "gfx/overworld/363.2bpp"
-INCBIN "gfx/overworld/364.2bpp"
-INCBIN "gfx/overworld/365.2bpp"
-INCBIN "gfx/overworld/366.2bpp"
-INCBIN "gfx/overworld/367.2bpp"
-INCBIN "gfx/overworld/368.2bpp"
-; c5c40
-
-BlackBeltSpriteGFX: ; c5c40
-INCBIN "gfx/overworld/369.2bpp"
-INCBIN "gfx/overworld/370.2bpp"
-INCBIN "gfx/overworld/371.2bpp"
-INCBIN "gfx/overworld/372.2bpp"
-INCBIN "gfx/overworld/373.2bpp"
-INCBIN "gfx/overworld/374.2bpp"
-; c5dc0
-
-ReceptionistSpriteGFX: ; c5dc0
-INCBIN "gfx/overworld/375.2bpp"
-INCBIN "gfx/overworld/376.2bpp"
-INCBIN "gfx/overworld/377.2bpp"
-INCBIN "gfx/overworld/378.2bpp"
-INCBIN "gfx/overworld/379.2bpp"
-INCBIN "gfx/overworld/380.2bpp"
-; c5f40
-
-OfficerSpriteGFX: ; c5f40
-INCBIN "gfx/overworld/381.2bpp"
-INCBIN "gfx/overworld/382.2bpp"
-INCBIN "gfx/overworld/383.2bpp"
-INCBIN "gfx/overworld/384.2bpp"
-INCBIN "gfx/overworld/385.2bpp"
-INCBIN "gfx/overworld/386.2bpp"
-; c60c0
-
-CalSpriteGFX: ; c60c0
-INCBIN "gfx/overworld/387.2bpp"
-INCBIN "gfx/overworld/388.2bpp"
-INCBIN "gfx/overworld/389.2bpp"
-INCBIN "gfx/overworld/390.2bpp"
-INCBIN "gfx/overworld/391.2bpp"
-INCBIN "gfx/overworld/392.2bpp"
-; c6240
-
-SlowpokeSpriteGFX: ; c6240
-INCBIN "gfx/overworld/393.2bpp"
-; c6280
-
-CaptainSpriteGFX: ; c6280
-INCBIN "gfx/overworld/394.2bpp"
-INCBIN "gfx/overworld/395.2bpp"
-INCBIN "gfx/overworld/396.2bpp"
-INCBIN "gfx/overworld/397.2bpp"
-INCBIN "gfx/overworld/398.2bpp"
-INCBIN "gfx/overworld/399.2bpp"
-; c6400
-
-BigLaprasSpriteGFX: ; c6400
-INCBIN "gfx/overworld/400.2bpp"
-INCBIN "gfx/overworld/401.2bpp"
-; c6480
-
-GymGuySpriteGFX: ; c6480
-INCBIN "gfx/overworld/402.2bpp"
-INCBIN "gfx/overworld/403.2bpp"
-INCBIN "gfx/overworld/404.2bpp"
-INCBIN "gfx/overworld/405.2bpp"
-INCBIN "gfx/overworld/406.2bpp"
-INCBIN "gfx/overworld/407.2bpp"
-; c6600
-
-SailorSpriteGFX: ; c6600
-INCBIN "gfx/overworld/408.2bpp"
-INCBIN "gfx/overworld/409.2bpp"
-INCBIN "gfx/overworld/410.2bpp"
-INCBIN "gfx/overworld/411.2bpp"
-INCBIN "gfx/overworld/412.2bpp"
-INCBIN "gfx/overworld/413.2bpp"
-; c6780
-
-BikerSpriteGFX: ; c6780
-INCBIN "gfx/overworld/414.2bpp"
-INCBIN "gfx/overworld/415.2bpp"
-INCBIN "gfx/overworld/416.2bpp"
-INCBIN "gfx/overworld/417.2bpp"
-INCBIN "gfx/overworld/418.2bpp"
-INCBIN "gfx/overworld/419.2bpp"
-; c6900
-
-PharmacistSpriteGFX: ; c6900
-INCBIN "gfx/overworld/420.2bpp"
-INCBIN "gfx/overworld/421.2bpp"
-INCBIN "gfx/overworld/422.2bpp"
-INCBIN "gfx/overworld/423.2bpp"
-INCBIN "gfx/overworld/424.2bpp"
-INCBIN "gfx/overworld/425.2bpp"
-; c6a80
-
-MonsterSpriteGFX: ; c6a80
-INCBIN "gfx/overworld/426.2bpp"
-INCBIN "gfx/overworld/427.2bpp"
-INCBIN "gfx/overworld/428.2bpp"
-INCBIN "gfx/overworld/429.2bpp"
-INCBIN "gfx/overworld/430.2bpp"
-INCBIN "gfx/overworld/431.2bpp"
-; c6c00
-
-FairySpriteGFX: ; c6c00
-INCBIN "gfx/overworld/432.2bpp"
-INCBIN "gfx/overworld/433.2bpp"
-INCBIN "gfx/overworld/434.2bpp"
-INCBIN "gfx/overworld/435.2bpp"
-INCBIN "gfx/overworld/436.2bpp"
-INCBIN "gfx/overworld/437.2bpp"
-; c6d80
-
-BirdSpriteGFX: ; c6d80
-INCBIN "gfx/overworld/438.2bpp"
-INCBIN "gfx/overworld/439.2bpp"
-INCBIN "gfx/overworld/440.2bpp"
-INCBIN "gfx/overworld/441.2bpp"
-INCBIN "gfx/overworld/442.2bpp"
-INCBIN "gfx/overworld/443.2bpp"
-; c6f00
-
-DragonSpriteGFX: ; c6f00
-INCBIN "gfx/overworld/444.2bpp"
-INCBIN "gfx/overworld/445.2bpp"
-INCBIN "gfx/overworld/446.2bpp"
-INCBIN "gfx/overworld/447.2bpp"
-INCBIN "gfx/overworld/448.2bpp"
-INCBIN "gfx/overworld/449.2bpp"
-; c7080
-
-BigOnixSpriteGFX: ; c7080
-INCBIN "gfx/overworld/450.2bpp"
-INCBIN "gfx/overworld/451.2bpp"
-INCBIN "gfx/overworld/452.2bpp"
-; c7140
-
-N64SpriteGFX: ; c7140
-INCBIN "gfx/overworld/453.2bpp"
-; c7180
-
-SudowoodoSpriteGFX: ; c7180
-INCBIN "gfx/overworld/454.2bpp"
-INCBIN "gfx/overworld/455.2bpp"
-; c7200
-
-SurfSpriteGFX: ; c7200
-INCBIN "gfx/overworld/456.2bpp"
-INCBIN "gfx/overworld/457.2bpp"
-INCBIN "gfx/overworld/458.2bpp"
-INCBIN "gfx/overworld/459.2bpp"
-INCBIN "gfx/overworld/460.2bpp"
-INCBIN "gfx/overworld/461.2bpp"
-; c7380
-
-PokeBallSpriteGFX: ; c7380
-INCBIN "gfx/overworld/462.2bpp"
-INCBIN "gfx/overworld/463.2bpp"
-INCBIN "gfx/overworld/464.2bpp"
-INCBIN "gfx/overworld/465.2bpp"
-INCBIN "gfx/overworld/466.2bpp"
-INCBIN "gfx/overworld/467.2bpp"
-; c7500
-
-PokedexSpriteGFX: ; c7500
-INCBIN "gfx/overworld/468.2bpp"
-INCBIN "gfx/overworld/469.2bpp"
-INCBIN "gfx/overworld/470.2bpp"
-INCBIN "gfx/overworld/471.2bpp"
-INCBIN "gfx/overworld/472.2bpp"
-INCBIN "gfx/overworld/473.2bpp"
-; c7680
-
-PaperSpriteGFX: ; c7680
-INCBIN "gfx/overworld/474.2bpp"
-INCBIN "gfx/overworld/475.2bpp"
-INCBIN "gfx/overworld/476.2bpp"
-INCBIN "gfx/overworld/477.2bpp"
-INCBIN "gfx/overworld/478.2bpp"
-INCBIN "gfx/overworld/479.2bpp"
-; c7800
-
-VirtualBoySpriteGFX: ; c7800
-INCBIN "gfx/overworld/480.2bpp"
-; c7840
-
-OldLinkReceptionistSpriteGFX: ; c7840
-INCBIN "gfx/overworld/481.2bpp"
-; c7880
-
-RockSpriteGFX: ; c7880
-INCBIN "gfx/overworld/482.2bpp"
-; c78c0
-
-BoulderSpriteGFX: ; c78c0
-INCBIN "gfx/overworld/483.2bpp"
-; c7900
-
-SnesSpriteGFX: ; c7900
-INCBIN "gfx/overworld/484.2bpp"
-; c7940
-
-FamicomSpriteGFX: ; c7940
-INCBIN "gfx/overworld/485.2bpp"
-; c7980
-
-FruitTreeSpriteGFX: ; c7980
-INCBIN "gfx/overworld/486.2bpp"
-; c79c0
-
-GoldTrophySpriteGFX: ; c79c0
-INCBIN "gfx/overworld/487.2bpp"
-; c7a00
-
-SilverTrophySpriteGFX: ; c7a00
-INCBIN "gfx/overworld/488.2bpp"
-; c7a40
-
-KrisSpriteGFX: ; c7a40
-INCBIN "gfx/overworld/489.2bpp"
-INCBIN "gfx/overworld/490.2bpp"
-INCBIN "gfx/overworld/491.2bpp"
-INCBIN "gfx/overworld/492.2bpp"
-INCBIN "gfx/overworld/493.2bpp"
-INCBIN "gfx/overworld/494.2bpp"
-; c7bc0
-
-KrisBikeSpriteGFX: ; c7bc0
-INCBIN "gfx/overworld/495.2bpp"
-INCBIN "gfx/overworld/496.2bpp"
-INCBIN "gfx/overworld/497.2bpp"
-INCBIN "gfx/overworld/498.2bpp"
-INCBIN "gfx/overworld/499.2bpp"
-INCBIN "gfx/overworld/500.2bpp"
-; c7d40
-
-KurtOutsideSpriteGFX: ; c7d40
-INCBIN "gfx/overworld/501.2bpp"
-INCBIN "gfx/overworld/502.2bpp"
-INCBIN "gfx/overworld/503.2bpp"
-; c7e00
-
-SuicuneSpriteGFX: ; c7e00
-INCBIN "gfx/overworld/504.2bpp"
-; c7e40
-
-EnteiSpriteGFX: ; c7e40
-INCBIN "gfx/overworld/505.2bpp"
-; c7e80
-
-RaikouSpriteGFX: ; c7e80
-INCBIN "gfx/overworld/506.2bpp"
-; c7ec0
-
-StandingYoungsterSpriteGFX: ; c7ec0
-INCBIN "gfx/overworld/507.2bpp"
-INCBIN "gfx/overworld/508.2bpp"
-INCBIN "gfx/overworld/509.2bpp"
-INCBIN "gfx/overworld/510.2bpp"
-INCBIN "gfx/overworld/511.2bpp"
-; c8000
-
+PokefanMSpriteGFX:            INCBIN "gfx/overworld/044.2bpp" ; c4000
+PokefanFSpriteGFX:            INCBIN "gfx/overworld/045.2bpp" ; c4180
+GrampsSpriteGFX:              INCBIN "gfx/overworld/046.2bpp" ; c4300
+GrannySpriteGFX:              INCBIN "gfx/overworld/047.2bpp" ; c4480
+SwimmerGuySpriteGFX:          INCBIN "gfx/overworld/048.2bpp" ; c4600
+SwimmerGirlSpriteGFX:         INCBIN "gfx/overworld/049.2bpp" ; c4780
+BigSnorlaxSpriteGFX:          INCBIN "gfx/overworld/050.2bpp" ; c4900
+SurfingPikachuSpriteGFX:      INCBIN "gfx/overworld/051.2bpp" ; c4980
+RocketSpriteGFX:              INCBIN "gfx/overworld/052.2bpp" ; c4b00
+RocketGirlSpriteGFX:          INCBIN "gfx/overworld/053.2bpp" ; c4c80
+NurseSpriteGFX:               INCBIN "gfx/overworld/054.2bpp" ; c4e00
+LinkReceptionistSpriteGFX:    INCBIN "gfx/overworld/055.2bpp" ; c4ec0
+ClerkSpriteGFX:               INCBIN "gfx/overworld/056.2bpp" ; c5040
+FisherSpriteGFX:              INCBIN "gfx/overworld/057.2bpp" ; c51c0
+FishingGuruSpriteGFX:         INCBIN "gfx/overworld/058.2bpp" ; c5340
+ScientistSpriteGFX:           INCBIN "gfx/overworld/059.2bpp" ; c54c0
+KimonoGirlSpriteGFX:          INCBIN "gfx/overworld/060.2bpp" ; c5640
+SageSpriteGFX:                INCBIN "gfx/overworld/061.2bpp" ; c57c0
+UnusedGuySpriteGFX:           INCBIN "gfx/overworld/062.2bpp" ; c5940
+GentlemanSpriteGFX:           INCBIN "gfx/overworld/063.2bpp" ; c5ac0
+BlackBeltSpriteGFX:           INCBIN "gfx/overworld/064.2bpp" ; c5c40
+ReceptionistSpriteGFX:        INCBIN "gfx/overworld/065.2bpp" ; c5dc0
+OfficerSpriteGFX:             INCBIN "gfx/overworld/066.2bpp" ; c5f40
+CalSpriteGFX:                 INCBIN "gfx/overworld/067.2bpp" ; c60c0
+SlowpokeSpriteGFX:            INCBIN "gfx/overworld/068.2bpp" ; c6240
+CaptainSpriteGFX:             INCBIN "gfx/overworld/069.2bpp" ; c6280
+BigLaprasSpriteGFX:           INCBIN "gfx/overworld/070.2bpp" ; c6400
+GymGuySpriteGFX:              INCBIN "gfx/overworld/071.2bpp" ; c6480
+SailorSpriteGFX:              INCBIN "gfx/overworld/072.2bpp" ; c6600
+BikerSpriteGFX:               INCBIN "gfx/overworld/073.2bpp" ; c6780
+PharmacistSpriteGFX:          INCBIN "gfx/overworld/074.2bpp" ; c6900
+MonsterSpriteGFX:             INCBIN "gfx/overworld/075.2bpp" ; c6a80
+FairySpriteGFX:               INCBIN "gfx/overworld/076.2bpp" ; c6c00
+BirdSpriteGFX:                INCBIN "gfx/overworld/077.2bpp" ; c6d80
+DragonSpriteGFX:              INCBIN "gfx/overworld/078.2bpp" ; c6f00
+BigOnixSpriteGFX:             INCBIN "gfx/overworld/079.2bpp" ; c7080
+N64SpriteGFX:                 INCBIN "gfx/overworld/080.2bpp" ; c7140
+SudowoodoSpriteGFX:           INCBIN "gfx/overworld/081.2bpp" ; c7180
+SurfSpriteGFX:                INCBIN "gfx/overworld/082.2bpp" ; c7200
+PokeBallSpriteGFX:            INCBIN "gfx/overworld/083.2bpp" ; c7380
+PokedexSpriteGFX:             INCBIN "gfx/overworld/084.2bpp" ; c7500
+PaperSpriteGFX:               INCBIN "gfx/overworld/085.2bpp" ; c7680
+VirtualBoySpriteGFX:          INCBIN "gfx/overworld/086.2bpp" ; c7800
+OldLinkReceptionistSpriteGFX: INCBIN "gfx/overworld/087.2bpp" ; c7840
+RockSpriteGFX:                INCBIN "gfx/overworld/088.2bpp" ; c7880
+BoulderSpriteGFX:             INCBIN "gfx/overworld/089.2bpp" ; c78c0
+SnesSpriteGFX:                INCBIN "gfx/overworld/090.2bpp" ; c7900
+FamicomSpriteGFX:             INCBIN "gfx/overworld/091.2bpp" ; c7940
+FruitTreeSpriteGFX:           INCBIN "gfx/overworld/092.2bpp" ; c7980
+GoldTrophySpriteGFX:          INCBIN "gfx/overworld/093.2bpp" ; c79c0
+SilverTrophySpriteGFX:        INCBIN "gfx/overworld/094.2bpp" ; c7a00
+KrisSpriteGFX:                INCBIN "gfx/overworld/095.2bpp" ; c7a40
+KrisBikeSpriteGFX:            INCBIN "gfx/overworld/096.2bpp" ; c7bc0
+KurtOutsideSpriteGFX:         INCBIN "gfx/overworld/097.2bpp" ; c7d40
+SuicuneSpriteGFX:             INCBIN "gfx/overworld/098.2bpp" ; c7e00
+EnteiSpriteGFX:               INCBIN "gfx/overworld/099.2bpp" ; c7e40
+RaikouSpriteGFX:              INCBIN "gfx/overworld/100.2bpp" ; c7e80
+StandingYoungsterSpriteGFX:   INCBIN "gfx/overworld/101.2bpp" ; c7ec0
