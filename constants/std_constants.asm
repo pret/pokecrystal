17a47bf54b82f8282bcbe66b845f935c7319238d
IIMarckus <iimarckus@gmail.com>
2015-06-23 14:42:00 -0600

    Forgot to add file containing std constants.

---
+++ constants/std_constants.asm
@@ -0,0 +1,13 @@
+	enum_start
+	enum pokecenternurse
+	enum difficultbookshelf
+	enum picturebookshelf
+	enum magazinebookshelf
+	enum teamrocketoath
+	enum incenseburner
+	enum merchandiseshelf
+	enum townmap
+	enum window
+	enum tv
+	enum homepage
+	enum radio
