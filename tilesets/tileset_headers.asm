5a61c9cdd0c932ec838ae30d04f8e69867451f60
yenatch <yenatch@gmail.com>
2015-06-26 10:46:28 -0700

    Use a macro for tileset headers.

--- tilesets/tileset_headers.asm
+++ tilesets/tileset_headers.asm
@@ -1,298 +1,48 @@
-Tileset00:: ; 0x4d596
-	dbw BANK(Tileset00GFX), Tileset00GFX
-	dbw BANK(Tileset00Meta), Tileset00Meta
-	dbw BANK(Tileset00Coll), Tileset00Coll
-	dw Tileset00Anim
-	dw $0000
-	dw Tileset00PalMap
+tileset: macro
+	dbw BANK(\1GFX),  \1GFX
+	dbw BANK(\1Meta), \1Meta
+	dbw BANK(\1Coll), \1Coll
+	dw \1Anim
+	dw NULL
+	dw \1PalMap
+endm
 
-Tileset01:: ; 0x4d5a5
-	dbw BANK(Tileset01GFX), Tileset01GFX
-	dbw BANK(Tileset01Meta), Tileset01Meta
-	dbw BANK(Tileset01Coll), Tileset01Coll
-	dw Tileset01Anim
-	dw $0000
-	dw Tileset01PalMap
-
-Tileset02: ; 0x4d5b4
-	dbw BANK(Tileset02GFX), Tileset02GFX
-	dbw BANK(Tileset02Meta), Tileset02Meta
-	dbw BANK(Tileset02Coll), Tileset02Coll
-	dw Tileset02Anim
-	dw $0000
-	dw Tileset02PalMap
-
-Tileset03: ; 0x4d5c3
-	dbw BANK(Tileset03GFX), Tileset03GFX
-	dbw BANK(Tileset03Meta), Tileset03Meta
-	dbw BANK(Tileset03Coll), Tileset03Coll
-	dw Tileset03Anim
-	dw $0000
-	dw Tileset03PalMap
-
-Tileset04: ; 0x4d5d2
-	dbw BANK(Tileset04GFX), Tileset04GFX
-	dbw BANK(Tileset04Meta), Tileset04Meta
-	dbw BANK(Tileset04Coll), Tileset04Coll
-	dw Tileset04Anim
-	dw $0000
-	dw Tileset04PalMap
-
-Tileset05: ; 0x4d5e1
-	dbw BANK(Tileset05GFX), Tileset05GFX
-	dbw BANK(Tileset05Meta), Tileset05Meta
-	dbw BANK(Tileset05Coll), Tileset05Coll
-	dw Tileset05Anim
-	dw $0000
-	dw Tileset05PalMap
-
-Tileset06: ; 0x4d5f0
-	dbw BANK(Tileset06GFX), Tileset06GFX
-	dbw BANK(Tileset06Meta), Tileset06Meta
-	dbw BANK(Tileset06Coll), Tileset06Coll
-	dw Tileset06Anim
-	dw $0000
-	dw Tileset06PalMap
-
-Tileset07: ; 0x4d5ff
-	dbw BANK(Tileset07GFX), Tileset07GFX
-	dbw BANK(Tileset07Meta), Tileset07Meta
-	dbw BANK(Tileset07Coll), Tileset07Coll
-	dw Tileset07Anim
-	dw $0000
-	dw Tileset07PalMap
-
-Tileset08: ; 0x4d60e
-	dbw BANK(Tileset08GFX), Tileset08GFX
-	dbw BANK(Tileset08Meta), Tileset08Meta
-	dbw BANK(Tileset08Coll), Tileset08Coll
-	dw Tileset08Anim
-	dw $0000
-	dw Tileset08PalMap
-
-Tileset09: ; 0x4d61d
-	dbw BANK(Tileset09GFX), Tileset09GFX
-	dbw BANK(Tileset09Meta), Tileset09Meta
-	dbw BANK(Tileset09Coll), Tileset09Coll
-	dw Tileset09Anim
-	dw $0000
-	dw Tileset09PalMap
-
-Tileset10: ; 0x4d62c
-	dbw BANK(Tileset10GFX), Tileset10GFX
-	dbw BANK(Tileset10Meta), Tileset10Meta
-	dbw BANK(Tileset10Coll), Tileset10Coll
-	dw Tileset10Anim
-	dw $0000
-	dw Tileset10PalMap
-
-Tileset11: ; 0x4d63b
-	dbw BANK(Tileset11GFX), Tileset11GFX
-	dbw BANK(Tileset11Meta), Tileset11Meta
-	dbw BANK(Tileset11Coll), Tileset11Coll
-	dw Tileset11Anim
-	dw $0000
-	dw Tileset11PalMap
-
-Tileset12: ; 0x4d64a
-	dbw BANK(Tileset12GFX), Tileset12GFX
-	dbw BANK(Tileset12Meta), Tileset12Meta
-	dbw BANK(Tileset12Coll), Tileset12Coll
-	dw Tileset12Anim
-	dw $0000
-	dw Tileset12PalMap
-
-Tileset13: ; 0x4d659
-	dbw BANK(Tileset13GFX), Tileset13GFX
-	dbw BANK(Tileset13Meta), Tileset13Meta
-	dbw BANK(Tileset13Coll), Tileset13Coll
-	dw Tileset13Anim
-	dw $0000
-	dw Tileset13PalMap
-
-Tileset14: ; 0x4d668
-	dbw BANK(Tileset14GFX), Tileset14GFX
-	dbw BANK(Tileset14Meta), Tileset14Meta
-	dbw BANK(Tileset14Coll), Tileset14Coll
-	dw Tileset14Anim
-	dw $0000
-	dw Tileset14PalMap
-
-Tileset15: ; 0x4d677
-	dbw BANK(Tileset15GFX), Tileset15GFX
-	dbw BANK(Tileset15Meta), Tileset15Meta
-	dbw BANK(Tileset15Coll), Tileset15Coll
-	dw Tileset15Anim
-	dw $0000
-	dw Tileset15PalMap
-
-Tileset16: ; 0x4d686
-	dbw BANK(Tileset16GFX), Tileset16GFX
-	dbw BANK(Tileset16Meta), Tileset16Meta
-	dbw BANK(Tileset16Coll), Tileset16Coll
-	dw Tileset16Anim
-	dw $0000
-	dw Tileset16PalMap
-
-Tileset17: ; 0x4d695
-	dbw BANK(Tileset17GFX), Tileset17GFX
-	dbw BANK(Tileset17Meta), Tileset17Meta
-	dbw BANK(Tileset17Coll), Tileset17Coll
-	dw Tileset17Anim
-	dw $0000
-	dw Tileset17PalMap
-
-Tileset18: ; 0x4d6a4
-	dbw BANK(Tileset18GFX), Tileset18GFX
-	dbw BANK(Tileset18Meta), Tileset18Meta
-	dbw BANK(Tileset18Coll), Tileset18Coll
-	dw Tileset18Anim
-	dw $0000
-	dw Tileset18PalMap
-
-Tileset19: ; 0x4d6b3
-	dbw BANK(Tileset19GFX), Tileset19GFX
-	dbw BANK(Tileset19Meta), Tileset19Meta
-	dbw BANK(Tileset19Coll), Tileset19Coll
-	dw Tileset19Anim
-	dw $0000
-	dw Tileset19PalMap
-
-Tileset20: ; 0x4d6c2
-	dbw BANK(Tileset20GFX), Tileset20GFX
-	dbw BANK(Tileset20Meta), Tileset20Meta
-	dbw BANK(Tileset20Coll), Tileset20Coll
-	dw Tileset20Anim
-	dw $0000
-	dw Tileset20PalMap
-
-Tileset21: ; 0x4d6d1
-	dbw BANK(Tileset21GFX), Tileset21GFX
-	dbw BANK(Tileset21Meta), Tileset21Meta
-	dbw BANK(Tileset21Coll), Tileset21Coll
-	dw Tileset21Anim
-	dw $0000
-	dw Tileset21PalMap
-
-Tileset22: ; 0x4d6e0
-	dbw BANK(Tileset22GFX), Tileset22GFX
-	dbw BANK(Tileset22Meta), Tileset22Meta
-	dbw BANK(Tileset22Coll), Tileset22Coll
-	dw Tileset22Anim
-	dw $0000
-	dw Tileset22PalMap
-
-Tileset23: ; 0x4d6ef
-	dbw BANK(Tileset23GFX), Tileset23GFX
-	dbw BANK(Tileset23Meta), Tileset23Meta
-	dbw BANK(Tileset23Coll), Tileset23Coll
-	dw Tileset23Anim
-	dw $0000
-	dw Tileset23PalMap
-
-Tileset24: ; 0x4d6fe
-	dbw BANK(Tileset24GFX), Tileset24GFX
-	dbw BANK(Tileset24Meta), Tileset24Meta
-	dbw BANK(Tileset24Coll), Tileset24Coll
-	dw Tileset24Anim
-	dw $0000
-	dw Tileset24PalMap
-
-Tileset25: ; 0x4d70d
-	dbw BANK(Tileset25GFX), Tileset25GFX
-	dbw BANK(Tileset25Meta), Tileset25Meta
-	dbw BANK(Tileset25Coll), Tileset25Coll
-	dw Tileset25Anim
-	dw $0000
-	dw Tileset25PalMap
-
-Tileset26: ; 0x4d71c
-	dbw BANK(Tileset26GFX), Tileset26GFX
-	dbw BANK(Tileset26Meta), Tileset26Meta
-	dbw BANK(Tileset26Coll), Tileset26Coll
-	dw Tileset26Anim
-	dw $0000
-	dw Tileset26PalMap
-
-Tileset27: ; 0x4d72b
-	dbw BANK(Tileset27GFX), Tileset27GFX
-	dbw BANK(Tileset27Meta), Tileset27Meta
-	dbw BANK(Tileset27Coll), Tileset27Coll
-	dw Tileset27Anim
-	dw $0000
-	dw Tileset27PalMap
-
-Tileset28: ; 0x4d73a
-	dbw BANK(Tileset28GFX), Tileset28GFX
-	dbw BANK(Tileset28Meta), Tileset28Meta
-	dbw BANK(Tileset28Coll), Tileset28Coll
-	dw Tileset28Anim
-	dw $0000
-	dw Tileset28PalMap
-
-Tileset29: ; 0x4d749
-	dbw BANK(Tileset29GFX), Tileset29GFX
-	dbw BANK(Tileset29Meta), Tileset29Meta
-	dbw BANK(Tileset29Coll), Tileset29Coll
-	dw Tileset29Anim
-	dw $0000
-	dw Tileset29PalMap
-
-Tileset30: ; 0x4d758
-	dbw BANK(Tileset30GFX), Tileset30GFX
-	dbw BANK(Tileset30Meta), Tileset30Meta
-	dbw BANK(Tileset30Coll), Tileset30Coll
-	dw Tileset30Anim
-	dw $0000
-	dw Tileset30PalMap
-
-Tileset31: ; 0x4d767
-	dbw BANK(Tileset31GFX), Tileset31GFX
-	dbw BANK(Tileset31Meta), Tileset31Meta
-	dbw BANK(Tileset31Coll), Tileset31Coll
-	dw Tileset31Anim
-	dw $0000
-	dw Tileset31PalMap
-
-Tileset32: ; 0x4d776
-	dbw BANK(Tileset32GFX), Tileset32GFX
-	dbw BANK(Tileset32Meta), Tileset32Meta
-	dbw BANK(Tileset32Coll), Tileset32Coll
-	dw Tileset32Anim
-	dw $0000
-	dw Tileset32PalMap
-
-Tileset33: ; 0x4d785
-	dbw BANK(Tileset33GFX), Tileset33GFX
-	dbw BANK(Tileset33Meta), Tileset33Meta
-	dbw BANK(Tileset33Coll), Tileset33Coll
-	dw Tileset33Anim
-	dw $0000
-	dw Tileset33PalMap
-
-Tileset34: ; 0x4d794
-	dbw BANK(Tileset34GFX), Tileset34GFX
-	dbw BANK(Tileset34Meta), Tileset34Meta
-	dbw BANK(Tileset34Coll), Tileset34Coll
-	dw Tileset34Anim
-	dw $0000
-	dw Tileset34PalMap
-
-Tileset35: ; 0x4d7a3
-	dbw BANK(Tileset35GFX), Tileset35GFX
-	dbw BANK(Tileset35Meta), Tileset35Meta
-	dbw BANK(Tileset35Coll), Tileset35Coll
-	dw Tileset35Anim
-	dw $0000
-	dw Tileset35PalMap
-
-Tileset36: ; 0x4d7b2
-	dbw BANK(Tileset36GFX), Tileset36GFX
-	dbw BANK(Tileset36Meta), Tileset36Meta
-	dbw BANK(Tileset36Coll), Tileset36Coll
-	dw Tileset36Anim
-	dw $0000
-	dw Tileset36PalMap
-
-; 0x4d7c1
-
+Tileset00::
+	tileset Tileset00
+Tileset01::
+	tileset Tileset01
+	tileset Tileset02
+	tileset Tileset03
+	tileset Tileset04
+	tileset Tileset05
+	tileset Tileset06
+	tileset Tileset07
+	tileset Tileset08
+	tileset Tileset09
+	tileset Tileset10
+	tileset Tileset11
+	tileset Tileset12
+	tileset Tileset13
+	tileset Tileset14
+	tileset Tileset15
+	tileset Tileset16
+	tileset Tileset17
+	tileset Tileset18
+	tileset Tileset19
+	tileset Tileset20
+	tileset Tileset21
+	tileset Tileset22
+	tileset Tileset23
+	tileset Tileset24
+	tileset Tileset25
+	tileset Tileset26
+	tileset Tileset27
+	tileset Tileset28
+	tileset Tileset29
+	tileset Tileset30
+	tileset Tileset31
+	tileset Tileset32
+	tileset Tileset33
+	tileset Tileset34
+	tileset Tileset35
+	tileset Tileset36
