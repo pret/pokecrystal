d02c9e39dd5138aa2d5e11178fcef7e2a58d4230
PikalaxALT <PikalaxALT@gmail.com>
2016-01-20 19:16:25 -0500

    BG pal constants, convert tileset pal maps to asm

--- constants/tilemap_constants.asm
+++ constants/tilemap_constants.asm
@@ -41,3 +41,13 @@ const_value SET 1
 	const TILESET_KABUTO_WORD_ROOM     ; 22
 	const TILESET_OMANYTE_WORD_ROOM    ; 23
 	const TILESET_AERODACTYL_WORD_ROOM ; 24
+
+	const_def
+	const PAL_BG_GRAY
+	const PAL_BG_RED
+	const PAL_BG_GREEN
+	const PAL_BG_WATER
+	const PAL_BG_YELLOW
+	const PAL_BG_BROWN
+	const PAL_BG_ROOF
+	const PAL_BG_TEXT
--- macros.asm
+++ macros.asm
@@ -15,6 +15,7 @@ INCLUDE "macros/rst.asm"
 INCLUDE "macros/mobile.asm"
 INCLUDE "macros/trainer.asm"
 INCLUDE "macros/trade_anim.asm"
+INCLUDE "macros/pals.asm"
 
 RGB: MACRO
 	dw ((\3) << 10) + ((\2) << 5) + (\1)
