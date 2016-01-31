52f9ec8dce461c6982f6a41af301ce5bb087d8bd
IIMarckus <iimarckus@gmail.com>
2016-01-30 20:53:27 -0700

    Remove misleading (unreferenced) labels.

--- data/base_stats.asm
+++ data/base_stats.asm
@@ -1,755 +1,253 @@
 BaseData0::
-BulbasaurBaseData:: ; 0x51424
 INCLUDE "data/base_stats/bulbasaur.asm"
-
 BaseData1::
-IvysaurBaseData:: ; 0x51444
 INCLUDE "data/base_stats/ivysaur.asm"
-
-VenusaurBaseData:: ; 0x51464
 INCLUDE "data/base_stats/venusaur.asm"
-
-CharmanderBaseData:: ; 0x51484
 INCLUDE "data/base_stats/charmander.asm"
-
-CharmeleonBaseData:: ; 0x514a4
 INCLUDE "data/base_stats/charmeleon.asm"
-
-CharizardBaseData:: ; 0x514c4
 INCLUDE "data/base_stats/charizard.asm"
-
-SquirtleBaseData:: ; 0x514e4
 INCLUDE "data/base_stats/squirtle.asm"
-
-WartortleBaseData:: ; 0x51504
 INCLUDE "data/base_stats/wartortle.asm"
-
-BlastoiseBaseData:: ; 0x51524
 INCLUDE "data/base_stats/blastoise.asm"
-
-CaterpieBaseData:: ; 0x51544
 INCLUDE "data/base_stats/caterpie.asm"
-
-MetapodBaseData:: ; 0x51564
 INCLUDE "data/base_stats/metapod.asm"
-
-ButterfreeBaseData:: ; 0x51584
 INCLUDE "data/base_stats/butterfree.asm"
-
-WeedleBaseData:: ; 0x515a4
 INCLUDE "data/base_stats/weedle.asm"
-
-KakunaBaseData:: ; 0x515c4
 INCLUDE "data/base_stats/kakuna.asm"
-
-BeedrillBaseData:: ; 0x515e4
 INCLUDE "data/base_stats/beedrill.asm"
-
-PidgeyBaseData:: ; 0x51604
 INCLUDE "data/base_stats/pidgey.asm"
-
-PidgeottoBaseData:: ; 0x51624
 INCLUDE "data/base_stats/pidgeotto.asm"
-
-PidgeotBaseData:: ; 0x51644
 INCLUDE "data/base_stats/pidgeot.asm"
-
-RattataBaseData:: ; 0x51664
 INCLUDE "data/base_stats/rattata.asm"
-
-RaticateBaseData:: ; 0x51684
 INCLUDE "data/base_stats/raticate.asm"
-
-SpearowBaseData:: ; 0x516a4
 INCLUDE "data/base_stats/spearow.asm"
-
-FearowBaseData:: ; 0x516c4
 INCLUDE "data/base_stats/fearow.asm"
-
-EkansBaseData:: ; 0x516e4
 INCLUDE "data/base_stats/ekans.asm"
-
-ArbokBaseData:: ; 0x51704
 INCLUDE "data/base_stats/arbok.asm"
-
-PikachuBaseData:: ; 0x51724
 INCLUDE "data/base_stats/pikachu.asm"
-
-RaichuBaseData:: ; 0x51744
 INCLUDE "data/base_stats/raichu.asm"
-
-SandshrewBaseData:: ; 0x51764
 INCLUDE "data/base_stats/sandshrew.asm"
-
-SandslashBaseData:: ; 0x51784
 INCLUDE "data/base_stats/sandslash.asm"
-
-Nidoran_FBaseData:: ; 0x517a4
 INCLUDE "data/base_stats/nidoran_f.asm"
-
-NidorinaBaseData:: ; 0x517c4
 INCLUDE "data/base_stats/nidorina.asm"
-
-NidoqueenBaseData:: ; 0x517e4
 INCLUDE "data/base_stats/nidoqueen.asm"
-
-Nidoran_MBaseData:: ; 0x51804
 INCLUDE "data/base_stats/nidoran_m.asm"
-
-NidorinoBaseData:: ; 0x51824
 INCLUDE "data/base_stats/nidorino.asm"
-
-NidokingBaseData:: ; 0x51844
 INCLUDE "data/base_stats/nidoking.asm"
-
-ClefairyBaseData:: ; 0x51864
 INCLUDE "data/base_stats/clefairy.asm"
-
-ClefableBaseData:: ; 0x51884
 INCLUDE "data/base_stats/clefable.asm"
-
-VulpixBaseData:: ; 0x518a4
 INCLUDE "data/base_stats/vulpix.asm"
-
-NinetalesBaseData:: ; 0x518c4
 INCLUDE "data/base_stats/ninetales.asm"
-
-JigglypuffBaseData:: ; 0x518e4
 INCLUDE "data/base_stats/jigglypuff.asm"
-
-WigglytuffBaseData:: ; 0x51904
 INCLUDE "data/base_stats/wigglytuff.asm"
-
-ZubatBaseData:: ; 0x51924
 INCLUDE "data/base_stats/zubat.asm"
-
-GolbatBaseData:: ; 0x51944
 INCLUDE "data/base_stats/golbat.asm"
-
-OddishBaseData:: ; 0x51964
 INCLUDE "data/base_stats/oddish.asm"
-
-GloomBaseData:: ; 0x51984
 INCLUDE "data/base_stats/gloom.asm"
-
-VileplumeBaseData:: ; 0x519a4
 INCLUDE "data/base_stats/vileplume.asm"
-
-ParasBaseData:: ; 0x519c4
 INCLUDE "data/base_stats/paras.asm"
-
-ParasectBaseData:: ; 0x519e4
 INCLUDE "data/base_stats/parasect.asm"
-
-VenonatBaseData:: ; 0x51a04
 INCLUDE "data/base_stats/venonat.asm"
-
-VenomothBaseData:: ; 0x51a24
 INCLUDE "data/base_stats/venomoth.asm"
-
-DiglettBaseData:: ; 0x51a44
 INCLUDE "data/base_stats/diglett.asm"
-
-DugtrioBaseData:: ; 0x51a64
 INCLUDE "data/base_stats/dugtrio.asm"
-
-MeowthBaseData:: ; 0x51a84
 INCLUDE "data/base_stats/meowth.asm"
-
-PersianBaseData:: ; 0x51aa4
 INCLUDE "data/base_stats/persian.asm"
-
-PsyduckBaseData:: ; 0x51ac4
 INCLUDE "data/base_stats/psyduck.asm"
-
-GolduckBaseData:: ; 0x51ae4
 INCLUDE "data/base_stats/golduck.asm"
-
-MankeyBaseData:: ; 0x51b04
 INCLUDE "data/base_stats/mankey.asm"
-
-PrimeapeBaseData:: ; 0x51b24
 INCLUDE "data/base_stats/primeape.asm"
-
-GrowlitheBaseData:: ; 0x51b44
 INCLUDE "data/base_stats/growlithe.asm"
-
-ArcanineBaseData:: ; 0x51b64
 INCLUDE "data/base_stats/arcanine.asm"
-
-PoliwagBaseData:: ; 0x51b84
 INCLUDE "data/base_stats/poliwag.asm"
-
-PoliwhirlBaseData:: ; 0x51ba4
 INCLUDE "data/base_stats/poliwhirl.asm"
-
-PoliwrathBaseData:: ; 0x51bc4
 INCLUDE "data/base_stats/poliwrath.asm"
-
-AbraBaseData:: ; 0x51be4
 INCLUDE "data/base_stats/abra.asm"
-
-KadabraBaseData:: ; 0x51c04
 INCLUDE "data/base_stats/kadabra.asm"
-
-AlakazamBaseData:: ; 0x51c24
 INCLUDE "data/base_stats/alakazam.asm"
-
-MachopBaseData:: ; 0x51c44
 INCLUDE "data/base_stats/machop.asm"
-
-MachokeBaseData:: ; 0x51c64
 INCLUDE "data/base_stats/machoke.asm"
-
-MachampBaseData:: ; 0x51c84
 INCLUDE "data/base_stats/machamp.asm"
-
-BellsproutBaseData:: ; 0x51ca4
 INCLUDE "data/base_stats/bellsprout.asm"
-
-WeepinbellBaseData:: ; 0x51cc4
 INCLUDE "data/base_stats/weepinbell.asm"
-
-VictreebelBaseData:: ; 0x51ce4
 INCLUDE "data/base_stats/victreebel.asm"
-
-TentacoolBaseData:: ; 0x51d04
 INCLUDE "data/base_stats/tentacool.asm"
-
-TentacruelBaseData:: ; 0x51d24
 INCLUDE "data/base_stats/tentacruel.asm"
-
-GeodudeBaseData:: ; 0x51d44
 INCLUDE "data/base_stats/geodude.asm"
-
-GravelerBaseData:: ; 0x51d64
 INCLUDE "data/base_stats/graveler.asm"
-
-GolemBaseData:: ; 0x51d84
 INCLUDE "data/base_stats/golem.asm"
-
-PonytaBaseData:: ; 0x51da4
 INCLUDE "data/base_stats/ponyta.asm"
-
-RapidashBaseData:: ; 0x51dc4
 INCLUDE "data/base_stats/rapidash.asm"
-
-SlowpokeBaseData:: ; 0x51de4
 INCLUDE "data/base_stats/slowpoke.asm"
-
-SlowbroBaseData:: ; 0x51e04
 INCLUDE "data/base_stats/slowbro.asm"
-
-MagnemiteBaseData:: ; 0x51e24
 INCLUDE "data/base_stats/magnemite.asm"
-
-MagnetonBaseData:: ; 0x51e44
 INCLUDE "data/base_stats/magneton.asm"
-
-Farfetch_DBaseData:: ; 0x51e64
 INCLUDE "data/base_stats/farfetch_d.asm"
-
-DoduoBaseData:: ; 0x51e84
 INCLUDE "data/base_stats/doduo.asm"
-
-DodrioBaseData:: ; 0x51ea4
 INCLUDE "data/base_stats/dodrio.asm"
-
-SeelBaseData:: ; 0x51ec4
 INCLUDE "data/base_stats/seel.asm"
-
-DewgongBaseData:: ; 0x51ee4
 INCLUDE "data/base_stats/dewgong.asm"
-
-GrimerBaseData:: ; 0x51f04
 INCLUDE "data/base_stats/grimer.asm"
-
-MukBaseData:: ; 0x51f24
 INCLUDE "data/base_stats/muk.asm"
-
-ShellderBaseData:: ; 0x51f44
 INCLUDE "data/base_stats/shellder.asm"
-
-CloysterBaseData:: ; 0x51f64
 INCLUDE "data/base_stats/cloyster.asm"
-
-GastlyBaseData:: ; 0x51f84
 INCLUDE "data/base_stats/gastly.asm"
-
-HaunterBaseData:: ; 0x51fa4
 INCLUDE "data/base_stats/haunter.asm"
-
-GengarBaseData:: ; 0x51fc4
 INCLUDE "data/base_stats/gengar.asm"
-
-OnixBaseData:: ; 0x51fe4
 INCLUDE "data/base_stats/onix.asm"
-
-DrowzeeBaseData:: ; 0x52004
 INCLUDE "data/base_stats/drowzee.asm"
-
-HypnoBaseData:: ; 0x52024
 INCLUDE "data/base_stats/hypno.asm"
-
-KrabbyBaseData:: ; 0x52044
 INCLUDE "data/base_stats/krabby.asm"
-
-KinglerBaseData:: ; 0x52064
 INCLUDE "data/base_stats/kingler.asm"
-
-VoltorbBaseData:: ; 0x52084
 INCLUDE "data/base_stats/voltorb.asm"
-
-ElectrodeBaseData:: ; 0x520a4
 INCLUDE "data/base_stats/electrode.asm"
-
-ExeggcuteBaseData:: ; 0x520c4
 INCLUDE "data/base_stats/exeggcute.asm"
-
-ExeggutorBaseData:: ; 0x520e4
 INCLUDE "data/base_stats/exeggutor.asm"
-
-CuboneBaseData:: ; 0x52104
 INCLUDE "data/base_stats/cubone.asm"
-
-MarowakBaseData:: ; 0x52124
 INCLUDE "data/base_stats/marowak.asm"
-
-HitmonleeBaseData:: ; 0x52144
 INCLUDE "data/base_stats/hitmonlee.asm"
-
-HitmonchanBaseData:: ; 0x52164
 INCLUDE "data/base_stats/hitmonchan.asm"
-
-LickitungBaseData:: ; 0x52184
 INCLUDE "data/base_stats/lickitung.asm"
-
-KoffingBaseData:: ; 0x521a4
 INCLUDE "data/base_stats/koffing.asm"
-
-WeezingBaseData:: ; 0x521c4
 INCLUDE "data/base_stats/weezing.asm"
-
-RhyhornBaseData:: ; 0x521e4
 INCLUDE "data/base_stats/rhyhorn.asm"
-
-RhydonBaseData:: ; 0x52204
 INCLUDE "data/base_stats/rhydon.asm"
-
-ChanseyBaseData:: ; 0x52224
 INCLUDE "data/base_stats/chansey.asm"
-
-TangelaBaseData:: ; 0x52244
 INCLUDE "data/base_stats/tangela.asm"
-
-KangaskhanBaseData:: ; 0x52264
 INCLUDE "data/base_stats/kangaskhan.asm"
-
-HorseaBaseData:: ; 0x52284
 INCLUDE "data/base_stats/horsea.asm"
-
-SeadraBaseData:: ; 0x522a4
 INCLUDE "data/base_stats/seadra.asm"
-
-GoldeenBaseData:: ; 0x522c4
 INCLUDE "data/base_stats/goldeen.asm"
-
-SeakingBaseData:: ; 0x522e4
 INCLUDE "data/base_stats/seaking.asm"
-
-StaryuBaseData:: ; 0x52304
 INCLUDE "data/base_stats/staryu.asm"
-
-StarmieBaseData:: ; 0x52324
 INCLUDE "data/base_stats/starmie.asm"
-
-Mr__MimeBaseData:: ; 0x52344
 INCLUDE "data/base_stats/mr__mime.asm"
-
-ScytherBaseData:: ; 0x52364
 INCLUDE "data/base_stats/scyther.asm"
-
-JynxBaseData:: ; 0x52384
 INCLUDE "data/base_stats/jynx.asm"
-
-ElectabuzzBaseData:: ; 0x523a4
 INCLUDE "data/base_stats/electabuzz.asm"
-
-MagmarBaseData:: ; 0x523c4
 INCLUDE "data/base_stats/magmar.asm"
-
-PinsirBaseData:: ; 0x523e4
 INCLUDE "data/base_stats/pinsir.asm"
-
-TaurosBaseData:: ; 0x52404
 INCLUDE "data/base_stats/tauros.asm"
-
-MagikarpBaseData:: ; 0x52424
 INCLUDE "data/base_stats/magikarp.asm"
-
-GyaradosBaseData:: ; 0x52444
 INCLUDE "data/base_stats/gyarados.asm"
-
-LaprasBaseData:: ; 0x52464
 INCLUDE "data/base_stats/lapras.asm"
-
-DittoBaseData:: ; 0x52484
 INCLUDE "data/base_stats/ditto.asm"
-
-EeveeBaseData:: ; 0x524a4
 INCLUDE "data/base_stats/eevee.asm"
-
-VaporeonBaseData:: ; 0x524c4
 INCLUDE "data/base_stats/vaporeon.asm"
-
-JolteonBaseData:: ; 0x524e4
 INCLUDE "data/base_stats/jolteon.asm"
-
-FlareonBaseData:: ; 0x52504
 INCLUDE "data/base_stats/flareon.asm"
-
-PorygonBaseData:: ; 0x52524
 INCLUDE "data/base_stats/porygon.asm"
-
-OmanyteBaseData:: ; 0x52544
 INCLUDE "data/base_stats/omanyte.asm"
-
-OmastarBaseData:: ; 0x52564
 INCLUDE "data/base_stats/omastar.asm"
-
-KabutoBaseData:: ; 0x52584
 INCLUDE "data/base_stats/kabuto.asm"
-
-KabutopsBaseData:: ; 0x525a4
 INCLUDE "data/base_stats/kabutops.asm"
-
-AerodactylBaseData:: ; 0x525c4
 INCLUDE "data/base_stats/aerodactyl.asm"
-
-SnorlaxBaseData:: ; 0x525e4
 INCLUDE "data/base_stats/snorlax.asm"
-
-ArticunoBaseData:: ; 0x52604
 INCLUDE "data/base_stats/articuno.asm"
-
-ZapdosBaseData:: ; 0x52624
 INCLUDE "data/base_stats/zapdos.asm"
-
-MoltresBaseData:: ; 0x52644
 INCLUDE "data/base_stats/moltres.asm"
-
-DratiniBaseData:: ; 0x52664
 INCLUDE "data/base_stats/dratini.asm"
-
-DragonairBaseData:: ; 0x52684
 INCLUDE "data/base_stats/dragonair.asm"
-
-DragoniteBaseData:: ; 0x526a4
 INCLUDE "data/base_stats/dragonite.asm"
-
-MewtwoBaseData:: ; 0x526c4
 INCLUDE "data/base_stats/mewtwo.asm"
-
-MewBaseData:: ; 0x526e4
 INCLUDE "data/base_stats/mew.asm"
-
-ChikoritaBaseData:: ; 0x52704
 INCLUDE "data/base_stats/chikorita.asm"
-
-BayleefBaseData:: ; 0x52724
 INCLUDE "data/base_stats/bayleef.asm"
-
-MeganiumBaseData:: ; 0x52744
 INCLUDE "data/base_stats/meganium.asm"
-
-CyndaquilBaseData:: ; 0x52764
 INCLUDE "data/base_stats/cyndaquil.asm"
-
-QuilavaBaseData:: ; 0x52784
 INCLUDE "data/base_stats/quilava.asm"
-
-TyphlosionBaseData:: ; 0x527a4
 INCLUDE "data/base_stats/typhlosion.asm"
-
-TotodileBaseData:: ; 0x527c4
 INCLUDE "data/base_stats/totodile.asm"
-
-CroconawBaseData:: ; 0x527e4
 INCLUDE "data/base_stats/croconaw.asm"
-
-FeraligatrBaseData:: ; 0x52804
 INCLUDE "data/base_stats/feraligatr.asm"
-
-SentretBaseData:: ; 0x52824
 INCLUDE "data/base_stats/sentret.asm"
-
-FurretBaseData:: ; 0x52844
 INCLUDE "data/base_stats/furret.asm"
-
-HoothootBaseData:: ; 0x52864
 INCLUDE "data/base_stats/hoothoot.asm"
-
-NoctowlBaseData:: ; 0x52884
 INCLUDE "data/base_stats/noctowl.asm"
-
-LedybaBaseData:: ; 0x528a4
 INCLUDE "data/base_stats/ledyba.asm"
-
-LedianBaseData:: ; 0x528c4
 INCLUDE "data/base_stats/ledian.asm"
-
-SpinarakBaseData:: ; 0x528e4
 INCLUDE "data/base_stats/spinarak.asm"
-
-AriadosBaseData:: ; 0x52904
 INCLUDE "data/base_stats/ariados.asm"
-
-CrobatBaseData:: ; 0x52924
 INCLUDE "data/base_stats/crobat.asm"
-
-ChinchouBaseData:: ; 0x52944
 INCLUDE "data/base_stats/chinchou.asm"
-
-LanturnBaseData:: ; 0x52964
 INCLUDE "data/base_stats/lanturn.asm"
-
-PichuBaseData:: ; 0x52984
 INCLUDE "data/base_stats/pichu.asm"
-
-CleffaBaseData:: ; 0x529a4
 INCLUDE "data/base_stats/cleffa.asm"
-
-IgglybuffBaseData:: ; 0x529c4
 INCLUDE "data/base_stats/igglybuff.asm"
-
-TogepiBaseData:: ; 0x529e4
 INCLUDE "data/base_stats/togepi.asm"
-
-TogeticBaseData:: ; 0x52a04
 INCLUDE "data/base_stats/togetic.asm"
-
-NatuBaseData:: ; 0x52a24
 INCLUDE "data/base_stats/natu.asm"
-
-XatuBaseData:: ; 0x52a44
 INCLUDE "data/base_stats/xatu.asm"
-
-MareepBaseData:: ; 0x52a64
 INCLUDE "data/base_stats/mareep.asm"
-
-FlaaffyBaseData:: ; 0x52a84
 INCLUDE "data/base_stats/flaaffy.asm"
-
-AmpharosBaseData:: ; 0x52aa4
 INCLUDE "data/base_stats/ampharos.asm"
-
-BellossomBaseData:: ; 0x52ac4
 INCLUDE "data/base_stats/bellossom.asm"
-
-MarillBaseData:: ; 0x52ae4
 INCLUDE "data/base_stats/marill.asm"
-
-AzumarillBaseData:: ; 0x52b04
 INCLUDE "data/base_stats/azumarill.asm"
-
-SudowoodoBaseData:: ; 0x52b24
 INCLUDE "data/base_stats/sudowoodo.asm"
-
-PolitoedBaseData:: ; 0x52b44
 INCLUDE "data/base_stats/politoed.asm"
-
-HoppipBaseData:: ; 0x52b64
 INCLUDE "data/base_stats/hoppip.asm"
-
-SkiploomBaseData:: ; 0x52b84
 INCLUDE "data/base_stats/skiploom.asm"
-
-JumpluffBaseData:: ; 0x52ba4
 INCLUDE "data/base_stats/jumpluff.asm"
-
-AipomBaseData:: ; 0x52bc4
 INCLUDE "data/base_stats/aipom.asm"
-
-SunkernBaseData:: ; 0x52be4
 INCLUDE "data/base_stats/sunkern.asm"
-
-SunfloraBaseData:: ; 0x52c04
 INCLUDE "data/base_stats/sunflora.asm"
-
-YanmaBaseData:: ; 0x52c24
 INCLUDE "data/base_stats/yanma.asm"
-
-WooperBaseData:: ; 0x52c44
 INCLUDE "data/base_stats/wooper.asm"
-
-QuagsireBaseData:: ; 0x52c64
 INCLUDE "data/base_stats/quagsire.asm"
-
-EspeonBaseData:: ; 0x52c84
 INCLUDE "data/base_stats/espeon.asm"
-
-UmbreonBaseData:: ; 0x52ca4
 INCLUDE "data/base_stats/umbreon.asm"
-
-MurkrowBaseData:: ; 0x52cc4
 INCLUDE "data/base_stats/murkrow.asm"
-
-SlowkingBaseData:: ; 0x52ce4
 INCLUDE "data/base_stats/slowking.asm"
-
-MisdreavusBaseData:: ; 0x52d04
 INCLUDE "data/base_stats/misdreavus.asm"
-
-UnownBaseData:: ; 0x52d24
 INCLUDE "data/base_stats/unown.asm"
-
-WobbuffetBaseData:: ; 0x52d44
 INCLUDE "data/base_stats/wobbuffet.asm"
-
-GirafarigBaseData:: ; 0x52d64
 INCLUDE "data/base_stats/girafarig.asm"
-
-PinecoBaseData:: ; 0x52d84
 INCLUDE "data/base_stats/pineco.asm"
-
-ForretressBaseData:: ; 0x52da4
 INCLUDE "data/base_stats/forretress.asm"
-
-DunsparceBaseData:: ; 0x52dc4
 INCLUDE "data/base_stats/dunsparce.asm"
-
-GligarBaseData:: ; 0x52de4
 INCLUDE "data/base_stats/gligar.asm"
-
-SteelixBaseData:: ; 0x52e04
 INCLUDE "data/base_stats/steelix.asm"
-
-SnubbullBaseData:: ; 0x52e24
 INCLUDE "data/base_stats/snubbull.asm"
-
-GranbullBaseData:: ; 0x52e44
 INCLUDE "data/base_stats/granbull.asm"
-
-QwilfishBaseData:: ; 0x52e64
 INCLUDE "data/base_stats/qwilfish.asm"
-
-ScizorBaseData:: ; 0x52e84
 INCLUDE "data/base_stats/scizor.asm"
-
-ShuckleBaseData:: ; 0x52ea4
 INCLUDE "data/base_stats/shuckle.asm"
-
-HeracrossBaseData:: ; 0x52ec4
 INCLUDE "data/base_stats/heracross.asm"
-
-SneaselBaseData:: ; 0x52ee4
 INCLUDE "data/base_stats/sneasel.asm"
-
-TeddiursaBaseData:: ; 0x52f04
 INCLUDE "data/base_stats/teddiursa.asm"
-
-UrsaringBaseData:: ; 0x52f24
 INCLUDE "data/base_stats/ursaring.asm"
-
-SlugmaBaseData:: ; 0x52f44
 INCLUDE "data/base_stats/slugma.asm"
-
-MagcargoBaseData:: ; 0x52f64
 INCLUDE "data/base_stats/magcargo.asm"
-
-SwinubBaseData:: ; 0x52f84
 INCLUDE "data/base_stats/swinub.asm"
-
-PiloswineBaseData:: ; 0x52fa4
 INCLUDE "data/base_stats/piloswine.asm"
-
-CorsolaBaseData:: ; 0x52fc4
 INCLUDE "data/base_stats/corsola.asm"
-
-RemoraidBaseData:: ; 0x52fe4
 INCLUDE "data/base_stats/remoraid.asm"
-
-OctilleryBaseData:: ; 0x53004
 INCLUDE "data/base_stats/octillery.asm"
-
-DelibirdBaseData:: ; 0x53024
 INCLUDE "data/base_stats/delibird.asm"
-
-MantineBaseData:: ; 0x53044
 INCLUDE "data/base_stats/mantine.asm"
-
-SkarmoryBaseData:: ; 0x53064
 INCLUDE "data/base_stats/skarmory.asm"
-
-HoundourBaseData:: ; 0x53084
 INCLUDE "data/base_stats/houndour.asm"
-
-HoundoomBaseData:: ; 0x530a4
 INCLUDE "data/base_stats/houndoom.asm"
-
-KingdraBaseData:: ; 0x530c4
 INCLUDE "data/base_stats/kingdra.asm"
-
-PhanpyBaseData:: ; 0x530e4
 INCLUDE "data/base_stats/phanpy.asm"
-
-DonphanBaseData:: ; 0x53104
 INCLUDE "data/base_stats/donphan.asm"
-
-Porygon2BaseData:: ; 0x53124
 INCLUDE "data/base_stats/porygon2.asm"
-
-StantlerBaseData:: ; 0x53144
 INCLUDE "data/base_stats/stantler.asm"
-
-SmeargleBaseData:: ; 0x53164
 INCLUDE "data/base_stats/smeargle.asm"
-
-TyrogueBaseData:: ; 0x53184
 INCLUDE "data/base_stats/tyrogue.asm"
-
-HitmontopBaseData:: ; 0x531a4
 INCLUDE "data/base_stats/hitmontop.asm"
-
-SmoochumBaseData:: ; 0x531c4
 INCLUDE "data/base_stats/smoochum.asm"
-
-ElekidBaseData:: ; 0x531e4
 INCLUDE "data/base_stats/elekid.asm"
-
-MagbyBaseData:: ; 0x53204
 INCLUDE "data/base_stats/magby.asm"
-
-MiltankBaseData:: ; 0x53224
 INCLUDE "data/base_stats/miltank.asm"
-
-BlisseyBaseData:: ; 0x53244
 INCLUDE "data/base_stats/blissey.asm"
-
-RaikouBaseData:: ; 0x53264
 INCLUDE "data/base_stats/raikou.asm"
-
-EnteiBaseData:: ; 0x53284
 INCLUDE "data/base_stats/entei.asm"
-
-SuicuneBaseData:: ; 0x532a4
 INCLUDE "data/base_stats/suicune.asm"
-
-LarvitarBaseData:: ; 0x532c4
 INCLUDE "data/base_stats/larvitar.asm"
-
-PupitarBaseData:: ; 0x532e4
 INCLUDE "data/base_stats/pupitar.asm"
-
-TyranitarBaseData:: ; 0x53304
 INCLUDE "data/base_stats/tyranitar.asm"
-
-LugiaBaseData:: ; 0x53324
 INCLUDE "data/base_stats/lugia.asm"
-
-Ho_OhBaseData:: ; 0x53344
 INCLUDE "data/base_stats/ho_oh.asm"
-
-CelebiBaseData:: ; 0x53364
 INCLUDE "data/base_stats/celebi.asm"
-
