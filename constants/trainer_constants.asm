b6c1ceed8087138ad050f458adf20bcd3297fdc0
surskitty <surskitty@gmail.com>
2017-11-27 00:35:10 -0500

    Constants for Rocket Grunt rosters.
    Having constants rather than magic numbers should make it easier
    to remove unused grunts from the roster.

--- constants/trainer_constants.asm
+++ constants/trainer_constants.asm
@@ -241,6 +241,37 @@ KRIS EQU __enum__
 	const MILLER
 
 	trainerclass GRUNTM ; 1f
+	const GRUNTM_1
+	const GRUNTM_2
+	const GRUNTM_3
+	const GRUNTM_4
+	const GRUNTM_5
+	const GRUNTM_6
+	const GRUNTM_7
+	const GRUNTM_8
+	const GRUNTM_9
+	const GRUNTM_10
+	const GRUNTM_11
+	const GRUNTM_12
+	const GRUNTM_13
+	const GRUNTM_14
+	const GRUNTM_15
+	const GRUNTM_16
+	const GRUNTM_17
+	const GRUNTM_18
+	const GRUNTM_19
+	const GRUNTM_20
+	const GRUNTM_21
+	const GRUNTM_22
+	const GRUNTM_23
+	const GRUNTM_24
+	const GRUNTM_25
+	const GRUNTM_26
+	const GRUNTM_27
+	const GRUNTM_28
+	const GRUNTM_29
+	const GRUNTM_30
+	const GRUNTM_31
 
 	trainerclass GENTLEMAN ; 20
 	const PRESTON
@@ -611,6 +642,12 @@ KRIS EQU __enum__
 	const DIRK
 
 	trainerclass GRUNTF ; 42
+	const GRUNTF_1
+	const GRUNTF_2
+	const GRUNTF_3
+	const GRUNTF_4
+	const GRUNTF_5
+
 
 	trainerclass MYSTICALMAN ; 43
 	const EUSINE
