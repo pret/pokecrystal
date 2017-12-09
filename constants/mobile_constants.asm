55d19b6dca382bc2f0dae9bef3ce95aa3907bc6a
Remy Oukaour <remy.oukaour@gmail.com>
2017-12-09 12:57:42 -0500

    Separate file for mobile constants

---
+++ constants/mobile_constants.asm
@@ -0,0 +1,21 @@
+; MobileEZChatCategoryPointers indexes (see misc/fixed_words.asm)
+	const_def
+	const EZCHAT_POKEMON
+	const EZCHAT_TYPES
+	const EZCHAT_GREETINGS
+	const EZCHAT_PEOPLE
+	const EZCHAT_BATTLE
+	const EZCHAT_EXCLAMATIONS
+	const EZCHAT_CONVERSATION
+	const EZCHAT_FEELINGS
+	const EZCHAT_CONDITIONS
+	const EZCHAT_LIFE
+	const EZCHAT_HOBBIES
+	const EZCHAT_ACTIONS
+	const EZCHAT_TIME
+	const EZCHAT_FAREWELLS
+	const EZCHAT_THISANDTHAT
+
+NUM_KANA EQU 45 ; length of SortedPokemon table (see misc/fixed_words.asm)
+
+MOBILE_LOGIN_PASSWORD_LENGTH EQU 17
