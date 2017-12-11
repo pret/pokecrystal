0f16ba854a3a53733e28fd61fea1b880d9bc1f4c
Remy Oukaour <remy.oukaour@gmail.com>
2017-12-10 20:05:34 -0500

    Comment palette colors and roof map groups

--- tilesets/roofs.asm
+++ tilesets/roofs.asm
@@ -19,17 +19,17 @@ LoadMapGroupRoof:: ; 1c000
 
 MapGroupRoofs: ; 1c021i
 	db -1 ; group 0 (unused)
-	db  3 ; group 1
-	db  2 ; group 2
+	db  3 ; group 1 (Olivine)
+	db  2 ; group 2 (Mahogany)
 	db -1 ; group 3
-	db  1 ; group 4
-	db  2 ; group 5
+	db  1 ; group 4 (Ecruteak)
+	db  2 ; group 5 (Blackthorn)
 	db -1 ; group 6
 	db -1 ; group 7
-	db  2 ; group 8
-	db  2 ; group 9
-	db  1 ; group 10
-	db  4 ; group 11
+	db  2 ; group 8 (Azalea)
+	db  2 ; group 9 (Lake of Rage)
+	db  1 ; group 10 (Violet)
+	db  4 ; group 11 (Goldenrod)
 	db -1 ; group 12
 	db -1 ; group 13
 	db -1 ; group 14
@@ -37,14 +37,14 @@ MapGroupRoofs: ; 1c021i
 	db -1 ; group 16
 	db -1 ; group 17
 	db -1 ; group 18
-	db  0 ; group 19
+	db  0 ; group 19 (Silver Cave)
 	db -1 ; group 20
 	db -1 ; group 21
-	db  3 ; group 22
+	db  3 ; group 22 (Cianwood)
 	db -1 ; group 23
-	db  0 ; group 24
+	db  0 ; group 24 (New Bark)
 	db -1 ; group 25
-	db  0 ; group 26
+	db  0 ; group 26 (Cherrygrove)
 ; 1c03c
 
 Roofs: ; 1c03c
