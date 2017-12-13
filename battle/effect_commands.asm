21607bdd1fc4950ab711bcc2f507add524d9d07b
mid-kid <esteve.varela@gmail.com>
2017-12-13 15:22:01 +0100

    HELD_TRADE_EVOLVE -> HELD_FLINCH
    This item effect effectively causes the target to flinch (sometimes).
    It's not exclusive to trade evolve items in later generations either.

--- battle/effect_commands.asm
+++ battle/effect_commands.asm
@@ -7184,7 +7184,7 @@ CheckOpponentWentFirst: ; 36abf
 ; 36ac9
 
 
-BattleCommand_KingsRock: ; 36ac9
+BattleCommand_HeldFlinch: ; 36ac9
 ; kingsrock
 
 	ld a, [AttackMissed]
@@ -7193,7 +7193,7 @@ BattleCommand_KingsRock: ; 36ac9
 
 	call GetUserItem
 	ld a, b
-	cp HELD_TRADE_EVOLVE ; Only King's Rock has this effect
+	cp HELD_FLINCH
 	ret nz
 
 	call CheckSubstituteOpp
