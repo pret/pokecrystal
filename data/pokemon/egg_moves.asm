1522140e418a3b4f9406a3c4e74a83454e378c1e
Remy Oukaour <remy.oukaour@gmail.com>
2017-12-24 18:23:24 -0500

    Clarify why G/S egg moves were removed (resolves #294)

--- data/pokemon/egg_moves.asm
+++ data/pokemon/egg_moves.asm
@@ -4,7 +4,9 @@ INCLUDE "includes.asm"
 SECTION "Egg Moves", ROMX
 
 ; All instances of Charm, Steel Wing, Sweet Scent, and Lovely Kiss were
-; removed from egg move lists in Crystal, because they are also TMs.
+; removed from egg move lists in Crystal.
+; Sweet Scent and Steel Wing were redundant since they're TMs, and
+; Charm and Lovely Kiss were unobtainable.
 
 ; Staryu's egg moves were removed in Crystal, because Staryu is genderless
 ; and can only breed with Ditto.
