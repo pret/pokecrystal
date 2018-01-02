e6778f931d929c1d4d867d58267aadb92ea4e129
Remy Oukaour <remy.oukaour@gmail.com>
2018-01-02 16:45:39 -0500

    town_map.png matches pokegear.png

--- data/palettes/town_map_palette_map.asm
+++ data/palettes/town_map_palette_map.asm
@@ -15,20 +15,16 @@ endr
 ENDM
 
 ; gfx/pokegear/town_map.png
-	townmappals EARTH,    EARTH,    EARTH,    MOUNTAIN, MOUNTAIN, MOUNTAIN
-	townmappals BORDER,   BORDER,   EARTH,    EARTH,    CITY,     EARTH
-	townmappals POI,      POI_MTN,  POI,      POI_MTN,  EARTH,    EARTH
-	townmappals EARTH,    MOUNTAIN, MOUNTAIN, MOUNTAIN, BORDER,   BORDER
-	townmappals EARTH,    EARTH,    BORDER,   EARTH,    EARTH,    BORDER
-	townmappals BORDER,   BORDER,   EARTH,    EARTH,    EARTH,    MOUNTAIN
-	townmappals MOUNTAIN, MOUNTAIN, BORDER,   BORDER,   BORDER,   BORDER
-	townmappals BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER
+	townmappals EARTH,    EARTH,    EARTH,    MOUNTAIN, MOUNTAIN, MOUNTAIN, BORDER,   BORDER
+	townmappals EARTH,    EARTH,    CITY,     EARTH,    POI,      POI_MTN,  POI,      POI_MTN
+	townmappals EARTH,    EARTH,    EARTH,    MOUNTAIN, MOUNTAIN, MOUNTAIN, BORDER,   BORDER
+	townmappals EARTH,    EARTH,    BORDER,   EARTH,    EARTH,    BORDER,   BORDER,   BORDER
+	townmappals EARTH,    EARTH,    EARTH,    MOUNTAIN, MOUNTAIN, MOUNTAIN, BORDER,   BORDER
+	townmappals BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER
 ; gfx/pokegear/pokegear.png
-	townmappals BORDER,   BORDER,   BORDER,   BORDER,   POI,      POI
-	townmappals POI,      BORDER,   BORDER,   BORDER,   BORDER,   BORDER
-	townmappals BORDER,   BORDER,   BORDER,   BORDER,   CITY,     CITY
-	townmappals CITY,     CITY,     CITY,     CITY,     CITY,     CITY
-	townmappals CITY,     CITY,     CITY,     CITY,     CITY,     CITY
-	townmappals CITY,     BORDER,   CITY,     CITY,     CITY,     CITY
-	townmappals CITY,     CITY,     CITY,     CITY,     BORDER,   BORDER
-	townmappals BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER
+	townmappals BORDER,   BORDER,   BORDER,   BORDER,   POI,      POI,      POI,      BORDER
+	townmappals BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER
+	townmappals CITY,     CITY,     CITY,     CITY,     CITY,     CITY,     CITY,     CITY
+	townmappals CITY,     CITY,     CITY,     CITY,     CITY,     CITY,     CITY,     BORDER
+	townmappals CITY,     CITY,     CITY,     CITY,     CITY,     CITY,     CITY,     CITY
+	townmappals BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER,   BORDER
