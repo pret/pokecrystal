5b7773b7d3ddf1addd99588cc5c41cd81ecdc402
mid-kid <esteve.varela@gmail.com>
2018-01-12 12:15:44 +0100

    Label frameset 09
    It seems to be used as the "big" text entry cursor (i.e. for the bottom
    3 buttons in the text entry screen).
    It is never referenced explicitly, instead used when adding 1 to
    SPRITEANIMSTRUCT_0E, in NamingScreen_AnimateCursor.

--- constants/sprite_data_constants.asm
+++ constants/sprite_data_constants.asm
@@ -1,4 +1,4 @@
-; overworld_sprite struct members (see gfx/sprite_data.asm)
+; overworld_sprite struct members (see data/sprites.asm)
 	const_def
 	const SPRITEDATA_ADDR    ; 0
 	const SPRITEDATA_ADDR_HI ; 1
