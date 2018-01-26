47e704b93163f0ae69d46425490426ad085643c7
Rangi <remy.oukaour+rangi42@gmail.com>
2018-01-26 11:39:35 -0500

    Correction, wave patterns are quite interesting.

--- audio/wave_samples.asm
+++ audio/wave_samples.asm
@@ -1,6 +1,6 @@
 WaveSamples: ; e8db2
-	; these are streams of 32 4-bit values used as wavepatterns
-	; nothing interesting here!
+	; These are streams of 32 nybbles (4-bit values) used as wave patterns.
+	; Plot them as a line chart to see the wave's shape.
 	dn  0,  2,  4,  6,  8, 10, 12, 14, 15, 15, 15, 14, 14, 13, 13, 12, 12, 11, 10,  9,  8,  7,  6,  5,  4,  4,  3,  3,  2,  2,  1,  1
 	dn  0,  2,  4,  6,  8, 10, 12, 14, 14, 15, 15, 15, 15, 14, 14, 14, 13, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  2,  1,  1
 	dn  1,  3,  6,  9, 11, 13, 14, 14, 14, 14, 15, 15, 15, 15, 14, 13, 13, 14, 15, 15, 15, 15, 14, 14, 14, 14, 13, 11,  9,  6,  3,  1
