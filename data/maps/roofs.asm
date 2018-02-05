f8f32941306615879b42b2af8894e65d82ee4a16
Rangi <remy.oukaour+rangi42@gmail.com>
2018-02-04 21:39:46 -0500

    Add ROOF_ constants

--- data/maps/roofs.asm
+++ data/maps/roofs.asm
@@ -1,36 +1,45 @@
+; MapGroupRoofs values; Roofs indexes
+	const_def
+	const ROOF_NEW_BARK  ; 0
+	const ROOF_VIOLET    ; 1
+	const ROOF_AZALEA    ; 2
+	const ROOF_OLIVINE   ; 3
+	const ROOF_GOLDENROD ; 4
+
 MapGroupRoofs: ; 1c021i
 ; entries correspond to map groups
 ; values are indexes for Roofs (see below)
-	db -1 ; group 0 (unused)
-	db  3 ; group 1 (Olivine)
-	db  2 ; group 2 (Mahogany)
-	db -1 ; group 3
-	db  1 ; group 4 (Ecruteak)
-	db  2 ; group 5 (Blackthorn)
-	db -1 ; group 6
-	db -1 ; group 7
-	db  2 ; group 8 (Azalea)
-	db  2 ; group 9 (Lake of Rage)
-	db  1 ; group 10 (Violet)
-	db  4 ; group 11 (Goldenrod)
-	db -1 ; group 12
-	db -1 ; group 13
-	db -1 ; group 14
-	db -1 ; group 15
-	db -1 ; group 16
-	db -1 ; group 17
-	db -1 ; group 18
-	db  0 ; group 19 (Silver Cave)
-	db -1 ; group 20
-	db -1 ; group 21
-	db  3 ; group 22 (Cianwood)
-	db -1 ; group 23
-	db  0 ; group 24 (New Bark)
-	db -1 ; group 25
-	db  0 ; group 26 (Cherrygrove)
+	db -1             ;  0
+	db ROOF_OLIVINE   ;  1 (Olivine)
+	db ROOF_AZALEA    ;  2 (Mahogany)
+	db -1             ;  3
+	db ROOF_VIOLET    ;  4 (Ecruteak)
+	db ROOF_AZALEA    ;  5 (Blackthorn)
+	db -1             ;  6
+	db -1             ;  7
+	db ROOF_AZALEA    ;  8 (Azalea)
+	db ROOF_AZALEA    ;  9 (Lake of Rage)
+	db ROOF_VIOLET    ; 10 (Violet)
+	db ROOF_GOLDENROD ; 11 (Goldenrod)
+	db -1             ; 12
+	db -1             ; 13
+	db -1             ; 14
+	db -1             ; 15
+	db -1             ; 16
+	db -1             ; 17
+	db -1             ; 18
+	db ROOF_NEW_BARK  ; 19 (Silver Cave)
+	db -1             ; 20
+	db -1             ; 21
+	db ROOF_OLIVINE   ; 22 (Cianwood)
+	db -1             ; 23
+	db ROOF_NEW_BARK  ; 24 (New Bark)
+	db -1             ; 25
+	db ROOF_NEW_BARK  ; 26 (Cherrygrove)
 ; 1c03c
 
 Roofs: ; 1c03c
+; entries correspond to ROOF_* constants
 INCBIN "gfx/tilesets/roofs/0.2bpp"
 INCBIN "gfx/tilesets/roofs/1.2bpp"
 INCBIN "gfx/tilesets/roofs/2.2bpp"
