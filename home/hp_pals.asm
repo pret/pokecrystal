4e776d4fa6bb0510fa06416ad7a6dd9ffe0ff724
Rangi <remy.oukaour+rangi42@gmail.com>
2018-05-23 11:14:15 -0400

    Use constants

--- home/hp_pals.asm
+++ home/hp_pals.asm
@@ -9,10 +9,10 @@ GetHPPal:: ; 3353
 ; Get palette for hp bar pixel length e in d.
 	ld d, HP_GREEN
 	ld a, e
-	cp (50 * 48 / 100)
+	cp (HP_BAR_LENGTH_PX * 50 / 100) ; 24
 	ret nc
 	inc d ; HP_YELLOW
-	cp (21 * 48 / 100)
+	cp (HP_BAR_LENGTH_PX * 21 / 100) ; 10
 	ret nc
 	inc d ; HP_RED
 	ret
