23450e6b4cd4d40735c5fabeb93f76f29c2ab0ed
Rangi <remy.oukaour+rangi42@gmail.com>
2018-07-05 18:59:56 -0400

    Correct file path

--- data/tilesets.asm
+++ data/tilesets.asm
@@ -8,7 +8,7 @@ ENDM
 ; Associated data:
 ; - The *GFX, *Meta, and *Coll are defined in gfx/tilesets.asm
 ; - The *PalMap are defined in gfx/tileset_palette_maps.asm
-; - The *Anim are defined in engine/tileset_anims.asm
+; - The *Anim are defined in engine/tilesets/tileset_anims.asm
 
 Tilesets::
 ; entries correspond to TILESET_* constants
