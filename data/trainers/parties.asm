f5cd6341fffddc4d244ea60df4b2c23e70d6041e
mid-kid <esteve.varela@gmail.com>
2018-08-07 20:53:14 +0200

    Consistency with order of constant comments

--- data/trainers/parties.asm
+++ data/trainers/parties.asm
@@ -5,8 +5,8 @@ Trainers:
 ; - db "NAME@", TRAINERTYPE_* constant
 ; - 1 to 6 Pokémon:
 ;    * for TRAINERTYPE_NORMAL:     db level, species
-;    * for TRAINERTYPE_ITEM:       db level, species, item
 ;    * for TRAINERTYPE_MOVES:      db level, species, 4 moves
+;    * for TRAINERTYPE_ITEM:       db level, species, item
 ;    * for TRAINERTYPE_ITEM_MOVES: db level, species, item, 4 moves
 ; - db -1 ; end
 
