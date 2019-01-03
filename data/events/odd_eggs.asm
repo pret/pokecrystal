b975f3e068fb9dbbe688445914cfb65036eeadc1
Rangi <remy.oukaour+rangi42@gmail.com>
2019-01-02 19:04:50 -0500

    Clean up some Odd Egg and Battle Tower code and data
    Fixes #589

--- data/events/odd_eggs.asm
+++ data/events/odd_eggs.asm
@@ -29,7 +29,6 @@ prob_total = 0
 
 OddEggs:
 
-OddEgg1:
 	db PICHU
 	db NO_ITEM
 	db THUNDERSHOCK, CHARM, DIZZY_PUNCH, 0
@@ -43,7 +42,7 @@ OddEgg1:
 	bigdw 0
 	dn 0, 0, 0, 0 ; DVs
 	db 30, 20, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -55,7 +54,6 @@ OddEgg1:
 	bigdw 8 ; SAtk
 	bigdw 8 ; SDef
 	db "EGG@@@@@@@@"
-OddEgg1End:
 
 	db PICHU
 	db NO_ITEM
@@ -70,7 +68,7 @@ OddEgg1End:
 	bigdw 0
 	dn 2, 10, 10, 10 ; DVs
 	db 30, 20, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -96,7 +94,7 @@ OddEgg1End:
 	bigdw 0
 	dn 0, 0, 0, 0 ; DVs
 	db 35, 20, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -122,7 +120,7 @@ OddEgg1End:
 	bigdw 0
 	dn 2, 10, 10, 10 ; DVs
 	db 35, 20, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -148,7 +146,7 @@ OddEgg1End:
 	bigdw 0
 	dn 0, 0, 0, 0 ; DVs
 	db 15, 20, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -174,7 +172,7 @@ OddEgg1End:
 	bigdw 0
 	dn 2, 10, 10, 10 ; DVs
 	db 15, 20, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -200,7 +198,7 @@ OddEgg1End:
 	bigdw 0
 	dn 0, 0, 0, 0 ; DVs
 	db 35, 30, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -226,7 +224,7 @@ OddEgg1End:
 	bigdw 0
 	dn 2, 10, 10, 10 ; DVs
 	db 35, 30, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -252,7 +250,7 @@ OddEgg1End:
 	bigdw 0
 	dn 0, 0, 0, 0 ; DVs
 	db 25, 10, 0, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -278,7 +276,7 @@ OddEgg1End:
 	bigdw 0
 	dn 2, 10, 10, 10 ; DVs
 	db 25, 10, 0, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -304,7 +302,7 @@ OddEgg1End:
 	bigdw 0
 	dn 0, 0, 0, 0 ; DVs
 	db 30, 30, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -330,7 +328,7 @@ OddEgg1End:
 	bigdw 0
 	dn 2, 10, 10, 10 ; DVs
 	db 30, 30, 10, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -356,7 +354,7 @@ OddEgg1End:
 	bigdw 0
 	dn 0, 0, 0, 0 ; DVs
 	db 35, 10, 0, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
@@ -382,7 +380,7 @@ OddEgg1End:
 	bigdw 0
 	dn 2, 10, 10, 10 ; DVs
 	db 35, 10, 0, 0 ; PP
-	db 20 ; Happiness
+	db 20 ; Step cycles to hatch
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 5 ; Level
 	db 0, 0 ; Status
