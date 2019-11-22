951e0dcd6b4c9c4fd0923375464e3e2209fc3d19
bbyers <bbyers@apptio.com>
2019-11-21 19:10:36 -0800

    Remove re-battling for now

--- engine/events/trainer_scripts.asm
+++ engine/events/trainer_scripts.asm
@@ -4,9 +4,6 @@ TalkToTrainerScript::
 	iftrue AlreadyBeatenTrainerScript
 	loadtemptrainer
 	encountermusic
-	opentext
-	trainertext TRAINERTEXT_SEEN
-	waitbutton
 	sjump StartBattleWithMapTrainerScript
 
 SeenByTrainerScript::
@@ -17,12 +14,12 @@ SeenByTrainerScript::
 	applymovementlasttalked wMovementBuffer
 	writeobjectxy LAST_TALKED
 	faceobject PLAYER, LAST_TALKED
-	opentext
-	trainertext TRAINERTEXT_SEEN
-	waitbutton
 	sjump StartBattleWithMapTrainerScript
 
 StartBattleWithMapTrainerScript:
+	opentext
+	trainertext TRAINERTEXT_SEEN
+	waitbutton
 	closetext
 	loadtemptrainer
 	startbattle
@@ -31,12 +28,4 @@ StartBattleWithMapTrainerScript:
 	loadmem wRunningTrainerBattleScript, -1
 
 AlreadyBeatenTrainerScript:
-; Say the post-battle message
-	loadtemptrainer
 	scripttalkafter
-	opentext
-	trainertext TRAINERTEXT_SEEN
-	waitbutton
-	yesorno
-	iftrue StartBattleWithMapTrainerScript
-	closetext
