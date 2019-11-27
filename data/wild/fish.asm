4eba1e66ce329e0c4ef7c71aad6ff7b405d2092e
bbyers <bbyers@apptio.com>
2019-11-27 14:10:16 -0800

    Set fishing hook rate to 75% from 50%

--- data/wild/fish.asm
+++ data/wild/fish.asm
@@ -7,19 +7,19 @@ ENDM
 
 FishGroups:
 ; entries correspond to FISHGROUP_* constants
-	fishgroup 50 percent + 1, .Shore_Old,            .Shore_Good,            .Shore_Super
-	fishgroup 50 percent + 1, .Ocean_Old,            .Ocean_Good,            .Ocean_Super
-	fishgroup 50 percent + 1, .Lake_Old,             .Lake_Good,             .Lake_Super
-	fishgroup 50 percent + 1, .Pond_Old,             .Pond_Good,             .Pond_Super
-	fishgroup 50 percent + 1, .Dratini_Old,          .Dratini_Good,          .Dratini_Super
-	fishgroup 50 percent + 1, .Qwilfish_Swarm_Old,   .Qwilfish_Swarm_Good,   .Qwilfish_Swarm_Super
-	fishgroup 50 percent + 1, .Remoraid_Swarm_Old,   .Remoraid_Swarm_Good,   .Remoraid_Swarm_Super
-	fishgroup 50 percent + 1, .Gyarados_Old,         .Gyarados_Good,         .Gyarados_Super
-	fishgroup 50 percent + 1, .Dratini_2_Old,        .Dratini_2_Good,        .Dratini_2_Super
-	fishgroup 50 percent + 1, .WhirlIslands_Old,     .WhirlIslands_Good,     .WhirlIslands_Super
-	fishgroup 50 percent + 1, .Qwilfish_Old,         .Qwilfish_Good,         .Qwilfish_Super
-	fishgroup 50 percent + 1, .Remoraid_Old,         .Remoraid_Good,         .Remoraid_Super
-	fishgroup 50 percent + 1, .Qwilfish_NoSwarm_Old, .Qwilfish_NoSwarm_Good, .Qwilfish_NoSwarm_Super
+	fishgroup 75 percent + 1, .Shore_Old,            .Shore_Good,            .Shore_Super
+	fishgroup 75 percent + 1, .Ocean_Old,            .Ocean_Good,            .Ocean_Super
+	fishgroup 75 percent + 1, .Lake_Old,             .Lake_Good,             .Lake_Super
+	fishgroup 75 percent + 1, .Pond_Old,             .Pond_Good,             .Pond_Super
+	fishgroup 75 percent + 1, .Dratini_Old,          .Dratini_Good,          .Dratini_Super
+	fishgroup 75 percent + 1, .Qwilfish_Swarm_Old,   .Qwilfish_Swarm_Good,   .Qwilfish_Swarm_Super
+	fishgroup 75 percent + 1, .Remoraid_Swarm_Old,   .Remoraid_Swarm_Good,   .Remoraid_Swarm_Super
+	fishgroup 75 percent + 1, .Gyarados_Old,         .Gyarados_Good,         .Gyarados_Super
+	fishgroup 75 percent + 1, .Dratini_2_Old,        .Dratini_2_Good,        .Dratini_2_Super
+	fishgroup 75 percent + 1, .WhirlIslands_Old,     .WhirlIslands_Good,     .WhirlIslands_Super
+	fishgroup 75 percent + 1, .Qwilfish_Old,         .Qwilfish_Good,         .Qwilfish_Super
+	fishgroup 75 percent + 1, .Remoraid_Old,         .Remoraid_Good,         .Remoraid_Super
+	fishgroup 75 percent + 1, .Qwilfish_NoSwarm_Old, .Qwilfish_NoSwarm_Good, .Qwilfish_NoSwarm_Super
 
 .Shore_Old:
 	db  70 percent + 1, MAGIKARP,   10
