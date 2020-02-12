ad5f7eed86e564701c5759cba09362078482e2be
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2020-02-12 14:33:51 -0800

    make encounter rates more even

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -4,15 +4,15 @@ mon_prob: MACRO
 ENDM
 
 GrassMonProbTable:
-	mon_prob 30,  0 ; 30% chance
-	mon_prob 60,  1 ; 30% chance
-	mon_prob 80,  2 ; 20% chance
-	mon_prob 90,  3 ; 10% chance
-	mon_prob 95,  4 ;  5% chance
-	mon_prob 99,  5 ;  4% chance
-	mon_prob 100, 6 ;  1% chance
+	mon_prob 20,  0 ; 20% chance
+	mon_prob 35,  1 ; 15% chance
+	mon_prob 50,  2 ; 15% chance
+	mon_prob 65,  3 ; 15% chance
+	mon_prob 80,  4 ; 15% chance
+	mon_prob 95,  5 ; 15% chance
+	mon_prob 100, 6 ;  5% chance
 
 WaterMonProbTable:
-	mon_prob 60,  0 ; 60% chance
-	mon_prob 90,  1 ; 30% chance
-	mon_prob 100, 2 ; 10% chance
+	mon_prob 40,  0 ; 40% chance
+	mon_prob 80,  1 ; 40% chance
+	mon_prob 100, 2 ; 20% chance
