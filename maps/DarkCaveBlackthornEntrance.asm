5d036b5b5a3d138a4d0eabd46c5b32a825269134
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2020-02-22 15:38:38 -0800

    Remove redundant ways of getting TMs

--- maps/DarkCaveBlackthornEntrance.asm
+++ maps/DarkCaveBlackthornEntrance.asm
@@ -29,7 +29,7 @@ DarkCaveBlackthornEntranceRevive:
 	itemball REVIVE
 
 DarkCaveBlackthornEntranceTMSnore:
-	itemball TM_SNORE
+	itemball AWAKENING
 
 DarkCaveBlackthornEntrancePharmacistText1:
 	text "Whoa! You startled"
