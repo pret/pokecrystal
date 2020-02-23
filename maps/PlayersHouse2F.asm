95467bf5f7ecdf2d6d6234c913116b55646e9e7d
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2020-02-23 07:50:07 -0800

    New Bark Radios don't check if you've received a pokemon from elm anymore

--- maps/PlayersHouse2F.asm
+++ maps/PlayersHouse2F.asm
@@ -51,33 +51,7 @@ PosterScript:
 	describedecoration DECODESC_POSTER
 
 PlayersHouseRadioScript:
-	checkevent EVENT_GOT_A_POKEMON_FROM_ELM
-	iftrue .NormalRadio
-	checkevent EVENT_LISTENED_TO_INITIAL_RADIO
-	iftrue .AbbreviatedRadio
-	playmusic MUSIC_POKEMON_TALK
-	opentext
-	writetext PlayersRadioText1
-	pause 45
-	writetext PlayersRadioText2
-	pause 45
-	writetext PlayersRadioText3
-	pause 45
-	musicfadeout MUSIC_NEW_BARK_TOWN, 16
-	writetext PlayersRadioText4
-	pause 45
-	closetext
-	setevent EVENT_LISTENED_TO_INITIAL_RADIO
-	end
-
-.NormalRadio:
 	jumpstd radio1
-
-.AbbreviatedRadio:
-	opentext
-	writetext PlayersRadioText4
-	pause 45
-	closetext
 	end
 
 PlayersHouseBookshelfScript:
--- maps/PlayersNeighborsHouse.asm
+++ maps/PlayersNeighborsHouse.asm
@@ -17,31 +17,7 @@ PlayersNeighborsHouseBookshelfScript:
 	jumpstd magazinebookshelf
 
 PlayersNeighborsHouseRadioScript:
-	checkevent EVENT_GOT_A_POKEMON_FROM_ELM
-	iftrue .NormalRadio
-	checkevent EVENT_LISTENED_TO_INITIAL_RADIO
-	iftrue .AbbreviatedRadio
-	playmusic MUSIC_POKEMON_TALK
-	opentext
-	writetext PlayerNeighborRadioText1
-	pause 45
-	writetext PlayerNeighborRadioText2
-	pause 45
-	writetext PlayerNeighborRadioText3
-	pause 45
-	musicfadeout MUSIC_NEW_BARK_TOWN, 16
-	writetext PlayerNeighborRadioText4
-	pause 45
-	closetext
-	setevent EVENT_LISTENED_TO_INITIAL_RADIO
-	end
-.NormalRadio:
 	jumpstd radio1
-.AbbreviatedRadio:
-	opentext
-	writetext PlayerNeighborRadioText4
-	pause 45
-	closetext
 	end
 
 PlayersNeighborsDaughterText:
