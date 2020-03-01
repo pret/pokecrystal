a46f9b78b2f24cd6064b8b0fd1b5f596ff2e6f8f
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2020-03-01 11:03:59 -0800

    Add Kanto leader pics for third trainer card page

--- engine/menus/trainer_card.asm
+++ engine/menus/trainer_card.asm
@@ -658,7 +658,7 @@ TrainerCard_KantoBadgesOAM:
 CardStatusGFX: INCBIN "gfx/trainer_card/card_status.2bpp"
 
 LeaderGFX:  INCBIN "gfx/trainer_card/leaders_johto.2bpp"
-LeaderGFX2: INCBIN "gfx/trainer_card/leaders_johto.2bpp"
+LeaderGFX2: INCBIN "gfx/trainer_card/leaders_kanto.2bpp"
 BadgeGFX:   INCBIN "gfx/trainer_card/badges.2bpp"
 BadgeGFX2:  INCBIN "gfx/trainer_card/badges.2bpp"
 
