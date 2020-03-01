c752d94476f43d768efe8005d3f1bae8ad02bf6a
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2020-02-29 18:26:22 -0800

    Add colored badges from pokecrystal tutorial by Rangi

--- engine/gfx/cgb_layouts.asm
+++ engine/gfx/cgb_layouts.asm
@@ -700,9 +700,11 @@ _CGB_TrainerCard:
 	ld a, PRYCE
 	call GetTrainerPalettePointer
 	call LoadPalette_White_Col1_Col2_Black
-	ld a, PREDEFPAL_CGB_BADGE
-	call GetPredefPal
-	call LoadHLPaletteIntoDE
+	
+	ld hl, .BadgePalettes
+	ld bc, 8 palettes
+	ld a, BANK(wOBPals1)
+	call FarCopyWRAM
 
 	; fill screen with opposite-gender palette for the card border
 	hlcoord 0, 0, wAttrmap
@@ -778,6 +780,9 @@ _CGB_TrainerCard:
 	ld a, $1
 	ldh [hCGBPalUpdate], a
 	ret
+
+.BadgePalettes
+INCLUDE "gfx/trainer_card/badges.pal"
 
 _CGB_MoveList:
 	ld de, wBGPals1
