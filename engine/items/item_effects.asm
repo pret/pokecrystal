f1524c5b629cc30bf1bed14849d38bffd149e268
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2020-03-03 19:39:42 -0800

    Gain exp after catching mon

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -499,6 +499,19 @@ PokeBallEffect:
 	ld hl, Text_GotchaMonWasCaught
 	call PrintText
 
+	ld a, [wTempSpecies]
+	ld l, a
+	ld a, [wCurPartyLevel]
+	ld h, a
+	push hl
+	farcall ApplyExperienceAfterEnemyCaught
+	pop hl
+	ld a, l
+	ld [wCurPartySpecies], a
+	ld [wTempSpecies], a
+	ld a, h
+	ld [wCurPartyLevel], a
+
 	call ClearSprites
 
 	ld a, [wTempSpecies]
