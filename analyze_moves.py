e404042b092a4ef28330e3ace1337715152fb471
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2020-04-03 19:07:37 -0700

    major cleanup

--- analyze_moves.py
+++ analyze_moves.py
@@ -12,119 +12,138 @@ Strongest Move Requirements
 - Stronger than all other moves in the same category that meet the above criteria
 '''
 
-# Define all of the moves that should be ignored while finding which move is "strongest"
-SACRIFICE_EFFECTS = ['EFFECT_SELFDESTRUCT']
-
-LEGENDARY_EXCLUSIVE_MOVES = ['SACRED_FIRE', 'AEROBLAST']
-COOLDOWN_REQUIRED_MOVES = ['HYPER_BEAM']
-SETUP_REQUIRED_MOVES = ['DREAM_EATER']
-MULTI_TURN_MOVES = ['FLY', 'SOLARBEAM', 'DIG', 'SKY_ATTACK']
-
-MOVE_EFFECTS_TO_IGNORE = SACRIFICE_EFFECTS
-MOVES_TO_IGNORE = LEGENDARY_EXCLUSIVE_MOVES \
-					+ COOLDOWN_REQUIRED_MOVES \
-					+ SETUP_REQUIRED_MOVES \
-					+ MULTI_TURN_MOVES
-
-class MoveInfo:
-	def __init__(self):
-		self.name = 'N/A'
-		self.power = 0
-		self.accuracy = 0
+class Move:
+	def __init__(self, name, power, accuracy, effect):
+		self.name = name
+		self.power = power
+		self.accuracy = accuracy
+		self.effect = effect
 
 class CategoryInfo:
+	def __init__(self, category_name):
+		self.category_name = category_name
+		self.strongest_move = Move('N/A', 0, 0, 'N/A')
+		self.moves_in_category_count = 0
+
+class StrongestMovesInType:
 	def __init__(self, name):
-		self.name = name
-		self.strongest_move = MoveInfo()
-		self.count = 0
-	
-	# TODO: move the functions below out of this class
-	def is_strongest_so_far(self, name, power, accuracy, effect):
-		return self.strongest_move.power < int(power) \
-				and int(accuracy) >= 90 \
-				and effect not in MOVE_EFFECTS_TO_IGNORE \
-				and name not in MOVES_TO_IGNORE
-
-	def updateInfo(self, name, power, accuracy, effect):
-		if(self.is_strongest_so_far(name, power, accuracy, effect)):
-			self.strongest_move.name = name
-			self.strongest_move.power = int(power)
-			self.strongest_move.accuracy = int(accuracy)
-			self.strongest_move.effect = effect
-		self.count += 1
-
-	def getFormattedInfo(self):
-		return '- Strongest Move:\n' \
-				+ ('  - Name: ' + self.strongest_move.name + '\n').replace('_', ' ').title() \
-				+ ('  - Power: ' + self.strongest_move.power) \
-				+ ('  - Accuracy: ' + self.strongest_move.accuracy) \
-				+ ('- Move Count: ' + str(self.count))
-
-class TypeInfo:
-	def __init__(self, name):
-		self.name = name
 		self.physical = CategoryInfo('Physical')
 		self.special = CategoryInfo('Special')
-		self.total = 0
 
-# Parse moves.asm, build dictionary of TypeInfo objects, and return them when finished
-def get_moves_by_type():
-	with open('data/moves/moves.asm', 'r', encoding='utf8') as move_file:
-		move_file_lines = move_file.readlines()
+class MoveFormatter:
 
-	# {{'ELECTRIC': TypeInfo()}, {'FIGHTING': TypeInfo()}}
-	moves_by_type = {}
+	# HORN_ATTACK -> Horn Attack
+	@staticmethod
+	def format_move_name(name):
+		return name.replace('_', ' ').title()
 
-	for move_file_line in move_file_lines:
+	@staticmethod
+	def format_move(move):
+		return 		('  - Name: ' 		+ 	MoveFormatter.format_move_name(move.name)	+ '\n') \
+				+ 	('  - Power: ' 		+ 	str(move.power) 							+ '\n') \
+				+ 	('  - Accuracy: '	+ 	str(move.accuracy) 							+ '\n')
+	
+	@staticmethod
+	def format_strongest_move_in_category(strongest_move_in_category):
+		return 		('### ' + strongest_move_in_category.category_name + '\n') \
+				+	('- Strongest Move:' + '\n') \
+				+	MoveFormatter.format_move(strongest_move_in_category.strongest_move) \
+				+	('- Move Count: ' + str(strongest_move_in_category.moves_in_category_count) + '\n')
 
-		# Only read move information from moves.asm
-		if move_file_line.startswith('\tmove '):
+	# TODO: separate formatting and exporting into two functions
+	@staticmethod
+	def format_and_export_strongest_moves_by_type(moves_by_type):
+		# Format and write results to file
+		with open('strongest_moves.md', 'w', encoding='utf8') as results_file:
+			results_file.write('# Strongest Moves\n')
+			for key in moves_by_type:
+				results_file.write('## ' + str(key).title() + ':\n')
 
-			# Get the name without the '\tmove and split line into an array'
-			move_attributes = move_file_line[6:].split(',')
+				results_file.write(MoveFormatter.format_strongest_move_in_category(moves_by_type[key].physical))
+				results_file.write(MoveFormatter.format_strongest_move_in_category(moves_by_type[key].special))
 
-			# Store values into labeled variables
-			move_name = move_attributes[0].strip()
-			move_effect = move_attributes[1].strip()
-			move_power = move_attributes[2].strip()
-			move_type = move_attributes[3].strip()
-			move_category = move_attributes[4].strip()
-			move_accuracy = move_attributes[5].strip()
-			# move_pp = move_attributes[6].strip()
-			# move_second_effect_chance = move_attributes[7].strip()
+				results_file.write('---\n')
 
-			# If dictionary entry for type doesn't exist yet, create one
-			dictVal = moves_by_type.get(move_type, -1)
-			if dictVal == -1:
-				moves_by_type[move_type] = TypeInfo(move_type)
+class MoveAnalyzer:
 
-			# Update the info for each category
-			if(move_category == 'PHYSICAL'):
-				moves_by_type[move_type].physical.updateInfo(move_name, move_power, move_accuracy, move_effect)
-			elif(move_category == 'SPECIAL'):
-				moves_by_type[move_type].special.updateInfo(move_name, move_power, move_accuracy, move_effect)
-			moves_by_type[move_type].total += 1
+	@staticmethod
+	def get_moves_to_ignore():
+		SACRIFICE_MOVES = ['EXPLOSION', 'SELFDESTRUCT']
+		LEGENDARY_EXCLUSIVE_MOVES = ['SACRED_FIRE', 'AEROBLAST']
+		COOLDOWN_REQUIRED_MOVES = ['HYPER_BEAM']
+		SETUP_REQUIRED_MOVES = ['DREAM_EATER']
+		MULTI_TURN_MOVES = ['FLY', 'SOLARBEAM', 'DIG', 'SKY_ATTACK']
 
-	return moves_by_type
 
-def format_and_export_move_analysis_by_type(moves_by_type):
-	# Format and write results to file
-	with open('strongest_moves.md', 'w', encoding='utf8') as results_file:
-		results_file.write('# Strongest Moves\n')
-		for key in moves_by_type:
-			results_file.write('## ' + str(key).title() + ':\n')
+		MOVES_TO_IGNORE = SACRIFICE_MOVES \
+							+ LEGENDARY_EXCLUSIVE_MOVES \
+							+ COOLDOWN_REQUIRED_MOVES \
+							+ SETUP_REQUIRED_MOVES \
+							+ MULTI_TURN_MOVES
+		return MOVES_TO_IGNORE
 
-			results_file.write('### Physical:\n' + moves_by_type[key].physical.getFormattedInfo() + '\n')
-			results_file.write('\n')
-			results_file.write('### Special:\n' + moves_by_type[key].special.getFormattedInfo() + '\n')
-			results_file.write('\n')
-			results_file.write('### Total Moves: ' + str(moves_by_type[key].total) + '\n')
-		
-			results_file.write('---\n')
+	@staticmethod
+	def is_move_stronger(move1, move2):
+		return int(move1.power) < int(move2.power) \
+				and int(move2.accuracy) >= 90 \
+				and move2.name not in MoveAnalyzer.get_moves_to_ignore()
+
+	@staticmethod
+	def calculate_stronger_move(move1, move2):
+		return move2 if(MoveAnalyzer.is_move_stronger(move1, move2)) else move1
+
+	# Parse moves.asm, build dictionary of StrongestMovesInType objects, and return them when finished
+	@staticmethod
+	def calculate_strongest_moves_per_type():
+		with open('data/moves/moves.asm', 'r', encoding='utf8') as move_file:
+			move_file_lines = move_file.readlines()
+
+		# {{'ELECTRIC': StrongestMovesInType()}, {'FIGHTING': StrongestMovesInType()}}
+		strongest_moves_by_type = {}
+
+		for move_file_line in move_file_lines:
+
+			# Only read move information from moves.asm
+			if move_file_line.startswith('\tmove '):
+
+				# Get the name without the '\tmove and split line into an array'
+				move_attributes = move_file_line[6:].split(',')
+
+				# Store values into labeled variables
+				move_name = move_attributes[0].strip()
+				move_effect = move_attributes[1].strip()
+				move_power = move_attributes[2].strip()
+				move_type = move_attributes[3].strip()
+				move_category = move_attributes[4].strip()
+				move_accuracy = move_attributes[5].strip()
+				# move_pp = move_attributes[6].strip()
+				# move_second_effect_chance = move_attributes[7].strip()
+
+				move_to_compare = Move(move_name, move_power, move_accuracy, move_effect)
+
+				# If dictionary entry for type doesn't exist yet, create one
+				dictVal = strongest_moves_by_type.get(move_type, -1)
+				if dictVal == -1:
+					strongest_moves_by_type[move_type] = StrongestMovesInType(move_type)
+
+				# Update the info for each category
+				if(move_category == 'PHYSICAL'):
+					current_strongest_move = strongest_moves_by_type[move_type].physical.strongest_move
+					strongest_move = MoveAnalyzer.calculate_stronger_move(current_strongest_move, move_to_compare)
+					strongest_moves_by_type[move_type].physical.strongest_move = strongest_move
+					strongest_moves_by_type[move_type].physical.moves_in_category_count += 1
+				elif(move_category == 'SPECIAL'):
+					current_strongest_move = strongest_moves_by_type[move_type].special.strongest_move
+					strongest_move = MoveAnalyzer.calculate_stronger_move(current_strongest_move, move_to_compare)
+					strongest_moves_by_type[move_type].special.strongest_move = strongest_move
+					strongest_moves_by_type[move_type].special.moves_in_category_count += 1
+
+		return strongest_moves_by_type
+
 
 def main():
-		move_analysis_by_type = get_moves_by_type()
-		format_and_export_move_analysis_by_type(move_analysis_by_type)
+		strongest_moves_by_type = MoveAnalyzer.calculate_strongest_moves_per_type()
+		MoveFormatter.format_and_export_strongest_moves_by_type(strongest_moves_by_type)
 
 if __name__ == '__main__':
 	main()
