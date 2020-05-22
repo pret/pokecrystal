37de310b4ddc4c53f10ea60a01a53a26567cf9bc
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2020-05-22 19:51:47 -0300

    Simplify the Makefile even more

--- gfx/lz.mk
+++ gfx/lz.mk
@@ -2,7 +2,7 @@
 
 %.lz: LZFLAGS = --method 80
 
-gfx/battle_anims/%.2bpp.lz: LZFLAGS = --method 82 --align 4
+gfx/battle_anims/%.lz: LZFLAGS = --method 82 --align 4
 
 gfx/battle/dude.2bpp.lz: LZFLAGS = --method 80 --align 4
 
@@ -10,38 +10,13 @@ gfx/diploma/diploma.2bpp.lz: LZFLAGS = --method 88 --align 1
 
 gfx/dummy_game/dummy_game.2bpp.lz: LZFLAGS = --method 82 --align 4
 
-gfx/intro/background.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/background.attrmap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/background.tilemap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/crystal_unowns.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/crystal_unowns.attrmap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/crystal_unowns.tilemap.lz: LZFLAGS = --method 80 --align 4
+gfx/intro/%.lz: LZFLAGS = --method 80 --align 4
 gfx/intro/logo.2bpp.lz: LZFLAGS = --method 82 --align 4
-gfx/intro/pichu_wooper.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/pulse.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/suicune_back.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/suicune_back.attrmap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/suicune_back.tilemap.lz: LZFLAGS = --method 80 --align 4
 gfx/intro/suicune_close.2bpp.lz: LZFLAGS = --method 82 --align 4
-gfx/intro/suicune_close.attrmap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/suicune_close.tilemap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/suicune_jump.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/suicune_jump.attrmap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/suicune_jump.tilemap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/suicune_run.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/unown_a.attrmap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/unown_a.tilemap.lz: LZFLAGS = --method 80 --align 4
 gfx/intro/unown_back.2bpp.lz: LZFLAGS = --method 82 --align 4
-gfx/intro/unown_hi.attrmap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/unown_hi.tilemap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/unowns.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/unowns.attrmap.lz: LZFLAGS = --method 80 --align 4
-gfx/intro/unowns.tilemap.lz: LZFLAGS = --method 80 --align 4
 
-gfx/mobile/mobile_trade.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/mobile/mobile_trade.attrmap.lz: LZFLAGS = --method 80 --align 4
+gfx/mobile/mobile_trade.%.lz: LZFLAGS = --method 80 --align 4
 gfx/mobile/mobile_trade_sprites.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/mobile/mobile_trade.tilemap.lz: LZFLAGS = --method 80 --align 4
 
 gfx/new_game/shrink1.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/new_game/shrink2.2bpp.lz: LZFLAGS = --method 80 --align 4
@@ -50,7 +25,7 @@ gfx/pc/pc.2bpp.lz: LZFLAGS = --method 73 --align 1
 
 gfx/player/chris_back.2bpp.lz: LZFLAGS = --method 80 --align 4
 
-gfx/pokedex/pokedex.2bpp.lz: LZFLAGS = --method 88
+gfx/pokedex/pokedex.2bpp.lz: LZFLAGS = --method 88 --align 1
 gfx/pokedex/pokedex_sgb.2bpp.lz: LZFLAGS = --method 88 --align 1
 gfx/pokedex/question_mark.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/pokedex/slowpoke.2bpp.lz: LZFLAGS = --method 73 --align 1
@@ -68,49 +43,25 @@ gfx/slots/slots_1.2bpp.lz: LZFLAGS = --method 82 --align 4
 gfx/slots/slots_2.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/slots/slots_3.2bpp.lz: LZFLAGS = --method 80 --align 4
 
+gfx/tilesets/%.lz: LZFLAGS = --method 80 --align 4
 gfx/tilesets/aerodactyl_word_room.2bpp.lz: LZFLAGS = --method 73 --align 1
-gfx/tilesets/battle_tower_inside.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/tilesets/battle_tower_outside.2bpp.lz: LZFLAGS = --method 73 --align 1
 gfx/tilesets/beta_word_room.2bpp.lz: LZFLAGS = --method 73 --align 1
-gfx/tilesets/cave.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/champions_room.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/dark_cave.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/elite_four_room.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/facility.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/forest.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/game_corner.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/gate.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/tilesets/ho_oh_word_room.2bpp.lz: LZFLAGS = --method 73 --align 1
 gfx/tilesets/house.2bpp.lz: LZFLAGS = --method 82 --align 4
-gfx/tilesets/ice_path.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/tilesets/johto.2bpp.lz: LZFLAGS = --method 73 --align 1
 gfx/tilesets/johto_modern.2bpp.lz: LZFLAGS = --method 73 --align 1
 gfx/tilesets/kabuto_word_room.2bpp.lz: LZFLAGS = --method 73 --align 1
 gfx/tilesets/kanto.2bpp.lz: LZFLAGS = --method 73 --align 1
-gfx/tilesets/lab.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/lighthouse.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/tilesets/mansion.2bpp.lz: LZFLAGS = --method 82 --align 4
 gfx/tilesets/mart.2bpp.lz: LZFLAGS = --method 82 --align 4
 gfx/tilesets/omanyte_word_room.2bpp.lz: LZFLAGS = --method 73 --align 1
-gfx/tilesets/park.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/players_house.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/players_room.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/pokecenter.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/pokecom_center.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/port.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/radio_tower.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/tilesets/ruins_of_alph.2bpp.lz: LZFLAGS = --method 73 --align 1
 gfx/tilesets/tower.2bpp.lz: LZFLAGS = --method 82 --align 4
-gfx/tilesets/traditional_house.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/train_station.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/tilesets/underground.2bpp.lz: LZFLAGS = --method 80 --align 4
 
 gfx/title/crystal.2bpp.lz: LZFLAGS = --method 80 --align 4
 gfx/title/logo.2bpp.lz: LZFLAGS = --method 73 --align 1
 gfx/title/suicune.2bpp.lz: LZFLAGS = --method 80 --align 4
 
-gfx/unown_puzzle/aerodactyl.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/unown_puzzle/hooh.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/unown_puzzle/kabuto.2bpp.lz: LZFLAGS = --method 80 --align 4
-gfx/unown_puzzle/omanyte.2bpp.lz: LZFLAGS = --method 80 --align 4
+gfx/unown_puzzle/%.lz: LZFLAGS = --method 80 --align 4
 gfx/unown_puzzle/start_cancel.2bpp.lz: LZFLAGS = --method 88 --align 1
