3550a87360308167d2751f17daa88c8ccd545e73
Rangi <remy.oukaour+rangi42@gmail.com>
2020-12-09 15:11:18 -0500

    Update rgbdscheck.asm
    Hard-code the version string in the failure message so it won't be in hex

--- rgbdscheck.asm
+++ rgbdscheck.asm
@@ -1,12 +1,14 @@
-; pokecrystal requires rgbds 0.4.1 or newer.
+; pokecrystal requires rgbds 0.4.2 or newer.
 MAJOR EQU 0
 MINOR EQU 4
-PATCH EQU 1
+PATCH EQU 2
 
 if !DEF(__RGBDS_MAJOR__) || !DEF(__RGBDS_MINOR__) || !DEF(__RGBDS_PATCH__)
-	fail "pokecrystal requires rgbds {MAJOR}.{MINOR}.{PATCH} or newer."
-elif (__RGBDS_MAJOR__ < MAJOR) || \
+	fail "pokecrystal requires rgbds 0.4.2 or newer."
+else
+if (__RGBDS_MAJOR__ < MAJOR) || \
 	(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ < MINOR) || \
 	(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ == MINOR && __RGBDS_PATCH__ < PATCH)
-	fail "pokecrystal requires rgbds {MAJOR}.{MINOR}.{PATCH} or newer."
+	fail "pokecrystal requires rgbds 0.4.2 or newer."
+endc
 endc
