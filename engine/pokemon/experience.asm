0f8027584b2d1686387c700879521f63b8f8aaa2
AliceDTRH <57547425+AliceDTRH@users.noreply.github.com>
2021-02-05 17:35:19 +0100

    Fix CalcExpAtLevel
    https://pret.github.io/pokecrystal/bugs_and_glitches.html#experience-underflow-for-level-1-pokmon-with-medium-slow-growth-rate

--- engine/pokemon/experience.asm
+++ engine/pokemon/experience.asm
@@ -32,6 +32,18 @@ CalcLevel:
 
 CalcExpAtLevel:
 ; (a/b)*n**3 + c*n**2 + d*n - e
+	ld a, d
+	dec a
+	jr nz, .UseExpFormula
+; Pokémon have 0 experience at level 1
+	ld hl, hProduct
+	ld [hli], a
+	ld [hli], a
+	ld [hli], a
+	ld [hl], a
+	ret
+
+.UseExpFormula
 	ld a, [wBaseGrowthRate]
 	add a
 	add a
