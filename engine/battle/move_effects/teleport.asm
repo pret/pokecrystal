2753244f79fb412ba6b3734d7ba45f3417b14b74
AliceDTRH <57547425+AliceDTRH@users.noreply.github.com>
2021-02-06 13:32:17 +0100

    Fix BattleCommand_Teleport

--- engine/battle/move_effects/teleport.asm
+++ engine/battle/move_effects/teleport.asm
@@ -66,9 +66,7 @@ BattleCommand_Teleport:
 	srl b
 	srl b
 	cp b
-	; This should be jr c, .failed
-	; As written, it makes enemy use of Teleport always succeed if able
-	jr nc, .run_away
+	jr c, .failed
 .run_away
 	call UpdateBattleMonInParty
 	xor a
