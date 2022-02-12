13837b429c16ab6c07c95ee8849750753ef5c903
Rangi <remy.oukaour+rangi42@gmail.com>
2022-02-12 10:01:12 -0500

    Define the `*_WILDDATA_LENGTH` equations to match their data representation

--- constants/pokemon_data_constants.asm
+++ constants/pokemon_data_constants.asm
@@ -172,8 +172,8 @@ NUM_HOF_TEAMS EQU 30
 NUM_GRASSMON EQU 7 ; data/wild/*_grass.asm table size
 NUM_WATERMON EQU 3 ; data/wild/*_water.asm table size
 
-GRASS_WILDDATA_LENGTH EQU 2 + (1 + NUM_GRASSMON * 2) * 3
-WATER_WILDDATA_LENGTH EQU 2 + (1 + NUM_WATERMON * 2) * 1
+GRASS_WILDDATA_LENGTH EQU 2 + 3 + NUM_GRASSMON * 2 * 3
+WATER_WILDDATA_LENGTH EQU 2 + 1 + NUM_WATERMON * 2
 FISHGROUP_DATA_LENGTH EQU 1 + 2 * 3
 
 NUM_ROAMMON_MAPS EQU 16 ; RoamMaps table size (see data/wild/roammon_maps.asm)
