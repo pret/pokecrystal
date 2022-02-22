17721908e9200af3b263efa58ed909c50fea52f4
vulcandth <vulcandth@gmail.com>
2022-02-22 12:15:34 -0600

    Remove double spacing from macros/vc.asm
    Co-authored-by: Rangi <35663410+Rangi42@users.noreply.github.com>

--- macros/vc.asm
+++ macros/vc.asm
@@ -3,15 +3,11 @@ vc_hook: MACRO
         .VC_\1::
     endc
 ENDM
-
-
 vc_patch: MACRO
     if DEF(_CRYSTALVC)
         .VC_\1::
     endc
 ENDM
-
-
 vc_assert: MACRO
     if DEF(_CRYSTALVC)
         assert \#
