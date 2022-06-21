ed060175cf7109e073ccc66d7e8818ec0fab040e
Colton G. Rushton <colton51919@gmail.com>
2022-06-20 21:58:52 -0300

    The removal of `DEF` for older rgbds

--- macros/scripts/modern_audio.asm
+++ macros/scripts/modern_audio.asm
@@ -5,24 +5,24 @@ transpose: MACRO
 	pitchoffset \1, \2 + 1
 ENDM
 
-DEF duty_cycle EQUS "dutycycle"
+duty_cycle EQUS "dutycycle"
 
-DEF sound_duty EQUS "unknownmusic0xde"
+sound_duty EQUS "unknownmusic0xde"
 
-DEF toggle_sfx EQUS "togglesfx"
+toggle_sfx EQUS "togglesfx"
 
-DEF toggle_noise EQUS "togglenoise"
+toggle_noise EQUS "togglenoise"
 
-DEF pitch_offset     EQUS "tone"
-DEF restart_channel  EQUS "restartchannel"
-DEF new_song         EQUS "newsong"
-DEF sfx_priority_on  EQUS "sfxpriorityon"
-DEF sfx_priority_off EQUS "sfxpriorityoff"
+pitch_offset     EQUS "tone"
+restart_channel  EQUS "restartchannel"
+new_song         EQUS "newsong"
+sfx_priority_on  EQUS "sfxpriorityon"
+sfx_priority_off EQUS "sfxpriorityoff"
 
-DEF sfx_toggle_noise EQUS "sfxtogglenoise"
-DEF set_condition    EQUS "setcondition"
-DEF sound_jump_if    EQUS "jumpif"
-DEF sound_jump       EQUS "jumpchannel"
-DEF sound_loop       EQUS "loopchannel"
-DEF sound_call       EQUS "callchannel"
-DEF sound_ret        EQUS "endchannel"
+sfx_toggle_noise EQUS "sfxtogglenoise"
+set_condition    EQUS "setcondition"
+sound_jump_if    EQUS "jumpif"
+sound_jump       EQUS "jumpchannel"
+sound_loop       EQUS "loopchannel"
+sound_call       EQUS "callchannel"
+sound_ret        EQUS "endchannel"
