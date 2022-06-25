b4d239840bdca1e3e0b616fb6f253f219df90347
Colton G. Rushton <colton51919@gmail.com>
2022-06-25 07:33:32 -0300

    Update engine/link/link.asm
    Co-authored-by: Eldred Habert <eldredhabert0@gmail.com>

--- engine/link/link.asm
+++ engine/link/link.asm
@@ -225,7 +225,7 @@ Gen2ToGen2LinkComms:
 	call DelayFrame
 	xor a
 	ldh [hSerialSend], a
-	ld a, 0 | SCF_SOURCE
+	ld a, SCF_SOURCE ; Stop any transfer in progress
 	ldh [rSC], a
 	ld a, SCF_START | SCF_SOURCE
 	ldh [rSC], a
