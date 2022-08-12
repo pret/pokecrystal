174c57e387b84dacfe13bbc4a197766d46dc47b9
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-08-11 22:14:48 -0500

    Add BUG comment

--- engine/menus/options_menu.asm
+++ engine/menus/options_menu.asm
@@ -11,6 +11,7 @@
 DEF NUM_OPTIONS EQU const_value ; 8
 
 _Option:
+; BUG: Options menu fails to clear joypad state on initialization (see docs/bugs_and_glitches.md)
 	ld hl, hInMenu
 	ld a, [hl]
 	push af
