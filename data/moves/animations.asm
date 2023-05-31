ae8042fbae29453c7dd310918c17e6cea8ff5fb1
vulcandth <vulcandth@gmail.com>
2023-05-31 13:31:37 +0000

    Use BattleAnim_Dummy

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -1,7 +1,7 @@
 BattleAnimations::
 ; entries correspond to constants/move_constants.asm
 	table_width 2, BattleAnimations
-	dw BattleAnim_0
+	dw BattleAnim_Dummy
 	dw BattleAnim_Pound
 	dw BattleAnim_KarateChop
 	dw BattleAnim_Doubleslap
@@ -120,7 +120,7 @@ BattleAnimations::
 	dw BattleAnim_FocusEnergy
 	dw BattleAnim_Bide
 	dw BattleAnim_Metronome
-	dw BattleAnim_MirrorMove
+	dw BattleAnim_Dummy
 	dw BattleAnim_Selfdestruct
 	dw BattleAnim_EggBomb
 	dw BattleAnim_Lick
@@ -254,9 +254,9 @@ BattleAnimations::
 	dw BattleAnim_Whirlpool
 	dw BattleAnim_BeatUp
 	assert_table_length NUM_ATTACKS + 1
-	dw BattleAnim_252
-	dw BattleAnim_253
-	dw BattleAnim_254
+	dw BattleAnim_Dummy
+	dw BattleAnim_Dummy
+	dw BattleAnim_Dummy
 	dw BattleAnim_SweetScent2
 	assert_table_length $100
 ; $100
@@ -284,11 +284,7 @@ BattleAnimations::
 	dw BattleAnim_HitConfusion
 	assert_table_length NUM_BATTLE_ANIMS + 1
 
-BattleAnim_0:
-BattleAnim_252:
-BattleAnim_253:
-BattleAnim_254:
-BattleAnim_MirrorMove:
+BattleAnim_Dummy:
 	anim_ret
 
 BattleAnim_SweetScent2:
