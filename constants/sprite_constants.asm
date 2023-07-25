8884cfb4ba4ebd888936d52cec4d026392c09fc7
EXGomora <dusttostardust@protonmail.com>
2023-07-25 10:00:02 -0400

    Added running animation
    Followed the new Running Shoes tutorial to add an animation for running. Disabled Pikachu Surfing.

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -107,6 +107,8 @@
 	const SPRITE_TM ; 67
 	const SPRITE_CHRIS_SURF ; 68
 	const SPRITE_KRIS_SURF ; 69
+	const SPRITE_CHRIS_RUN ; 6a
+	const SPRITE_KRIS_RUN ; 6b
 DEF NUM_OVERWORLD_SPRITES EQU const_value - 1
 
 ; SpriteMons indexes (see data/sprites/sprite_mons.asm)
--- engine/overworld/player_movement.asm
+++ engine/overworld/player_movement.asm
@@ -258,8 +258,6 @@ DoPlayerMovement::
 	ld a, [wPlayerState]
 	cp PLAYER_SURF
 	jr z, .TrySurf
-	cp PLAYER_SURF_PIKA
-	jr z, .TrySurf
 
 	call .CheckLandPerms
 	jr c, .bump
@@ -276,7 +274,7 @@ DoPlayerMovement::
 
 ; Downhill riding is slower when not moving down.
 	call .BikeCheck
-	jr nz, .walk
+	jr nz, .HandleWalkAndRun
 
 	ld hl, wBikeFlags
 	bit BIKEFLAGS_DOWNHILL_F, [hl]
@@ -298,9 +296,6 @@ DoPlayerMovement::
 	ret
 
 .walk
-	ld a, [wCurInput]
-	and B_BUTTON
-	jr nz, .run
 	ld a, STEP_WALK
 	call .DoStep
 	scf
@@ -326,6 +321,25 @@ DoPlayerMovement::
 .bump
 	xor a
 	ret
+
+.HandleWalkAndRun
+	ld a, [wWalkingDirection]
+	cp STANDING
+	jr z, .ensurewalk
+	ldh a, [hJoypadDown]
+	and B_BUTTON
+	cp B_BUTTON
+	jr nz, .ensurewalk
+	ld a, [wPlayerState]
+	cp PLAYER_RUN
+	call nz, .StartRunning
+	jr .run
+
+.ensurewalk
+	ld a, [wPlayerState]
+	cp PLAYER_NORMAL
+	call nz, .StartWalking
+	jr .walk
 
 .TrySurf:
 	call .CheckSurfPerms
@@ -807,6 +821,22 @@ ENDM
 	ld a, PLAYER_NORMAL
 	ld [wPlayerState], a
 	call UpdatePlayerSprite ; UpdateSprites
+	pop bc
+	ret
+
+.StartRunning:
+	ld a, PLAYER_RUN
+	ld [wPlayerState], a
+	push bc
+	farcall UpdatePlayerSprite
+	pop bc
+	ret
+
+.StartWalking:
+	ld a, PLAYER_NORMAL
+	ld [wPlayerState], a
+	push bc
+	farcall UpdatePlayerSprite
 	pop bc
 	ret
 
