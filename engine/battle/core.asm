804fa846e0f9c6d70507b0c402e9b3f583030339
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-01-01 21:46:23 -0500

    Fix bug doc formatting

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -143,11 +143,11 @@ WildFled_EnemyFled_LinkBattleCanceled:
 	call StdBattleTextbox
 
 .skip_text
-; BUG: SFX_RUN does not play correctly when a wild Pokemon flees from battle (see docs/bugs_and_glitches.md)
 	call StopDangerSound
 	call CheckMobileBattleError
 	jr c, .skip_sfx
 
+; BUG: SFX_RUN does not play correctly when a wild Pokémon flees from battle (see docs/bugs_and_glitches.md)
 	ld de, SFX_RUN
 	call PlaySFX
 
