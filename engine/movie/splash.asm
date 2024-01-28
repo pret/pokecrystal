c88f3b517048dd398ceaec549270ba4ba556d2d4
vulcandth <vulcandth@gmail.com>
2024-01-28 13:46:52 -0600

    Fix/update sprite y-coord offscreen definitions

--- engine/movie/splash.asm
+++ engine/movie/splash.asm
@@ -93,7 +93,7 @@ GameFreakPresentsInit:
 	call InitSpriteAnimStruct
 	ld hl, SPRITEANIMSTRUCT_YOFFSET
 	add hl, bc
-	ld [hl], 160
+	ld [hl], OAM_YCOORD_HIDDEN
 	ld hl, SPRITEANIMSTRUCT_VAR1
 	add hl, bc
 	ld [hl], 96
