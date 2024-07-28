80bd9422655b933d8ebd035b85fa30116d040a8b
Metallicity <fourequalsthree@gmail.com>
2024-07-27 22:24:01 -0700

    Rename Bill's brother to Bill's older sister

--- data/maps/blocks.asm
+++ data/maps/blocks.asm
@@ -174,7 +174,7 @@ VermilionDiglettsCaveSpeechHouse_Blocks:
 BluesHouse_Blocks:
 PewterNidoranSpeechHouse_Blocks:
 PewterSnoozeSpeechHouse_Blocks:
-BillsBrothersHouse_Blocks:
+BillsOlderSistersHouse_Blocks:
 LavenderSpeechHouse_Blocks:
 LavenderNameRater_Blocks:
 Route12SuperRodHouse_Blocks:
