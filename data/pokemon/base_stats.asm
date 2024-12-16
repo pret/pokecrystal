26e9de55d6b57385edabeb2fc55cc233e592bc70
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2024-12-16 10:48:48 -0500

    Fix indentation

--- data/pokemon/base_stats.asm
+++ data/pokemon/base_stats.asm
@@ -7,9 +7,9 @@ MACRO tmhm
 	; set bits of bytes
 	rept _NARG
 		if DEF(\1_TMNUM)
-		DEF n = (\1_TMNUM - 1) / 8
-		DEF i = (\1_TMNUM - 1) % 8
-		DEF _tm{d:n} |= 1 << i
+			DEF n = (\1_TMNUM - 1) / 8
+			DEF i = (\1_TMNUM - 1) % 8
+			DEF _tm{d:n} |= 1 << i
 		else
 			fail "\1 is not a TM, HM, or tutor move"
 		endc
