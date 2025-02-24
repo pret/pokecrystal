7a7a06289c70a39960746282242001f6afb23443
Darsh <55897426+itsdarsh@users.noreply.github.com>
2025-02-24 12:40:50 -0500

    Add `assert` to note `_CGB_Diploma` also loads `PartyMenuOBPals` (#1167)

--- engine/gfx/cgb_layouts.asm
+++ engine/gfx/cgb_layouts.asm
@@ -509,6 +509,7 @@ _CGB_BetaPoker:
 _CGB_Diploma:
 	ld hl, DiplomaPalettes
 	ld de, wBGPals1
+	assert DiplomaPalettes + 8 palettes == PartyMenuOBPals
 	ld bc, 16 palettes
 	ld a, BANK(wBGPals1)
 	call FarCopyWRAM
