73b019c68d0605cae17e79b3743b03e47867bbc1
Rangi <sylvie.oukaour+rangi42@gmail.com>
2025-12-01 14:07:55 -0500

    Don't use `list_start`/`assert_list_length` for roammon maps, since `li` is not involved

--- data/wild/roammon_maps.asm
+++ data/wild/roammon_maps.asm
@@ -1,6 +1,8 @@
 ; Maps that roaming monsters can be on, and possible maps they can jump to.
 ; Notably missing are Route 40 and Route 41, which are water routes.
 
+DEF __roam_maps__ = 0
+
 MACRO roam_map
 	map_id \1
 	db _NARG - 1
@@ -9,11 +11,10 @@ MACRO roam_map
 		shift
 	endr
 	db 0
-	DEF list_index += 1
+	DEF __roam_maps__ += 1
 ENDM
 
 RoamMaps:
-	list_start
 	; start map, ...other maps
 	roam_map ROUTE_29, ROUTE_30, ROUTE_46
 	roam_map ROUTE_30, ROUTE_29, ROUTE_31
@@ -31,5 +32,7 @@ RoamMaps:
 	roam_map ROUTE_44, ROUTE_42, ROUTE_43, ROUTE_45
 	roam_map ROUTE_45, ROUTE_44, ROUTE_46
 	roam_map ROUTE_46, ROUTE_45, ROUTE_29
-	assert_list_length NUM_ROAMMON_MAPS
 	db -1 ; end
+
+	assert __roam_maps__ == NUM_ROAMMON_MAPS, \
+		"RoamMaps: expected {d:NUM_ROAMMON_MAPS} entries, got {d:__roam_maps__}"
