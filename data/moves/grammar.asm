53880a1737773ebf17ba8aef7072843d23330bb5
SnorlaxMonster <snorlaxmonster@users.noreply.github.com>
2026-01-17 18:42:13 +1030

    Correct original Japanese text in comments

--- data/moves/grammar.asm
+++ data/moves/grammar.asm
@@ -3,7 +3,7 @@
 ; Made redundant in English localization, where all are just "[mon]<LINE>used [move]!"
 
 MoveGrammar:
-; 0: originally "[mon]の<LINE>[move]を　つかった！" ("[mon]<LINE>used [move]!")
+; 0: originally "[mon]は<LINE>[move]を　つかった！" ("[mon]<LINE>used [move]!")
 	db SWORDS_DANCE
 	db GROWTH
 	db STRENGTH
@@ -35,7 +35,7 @@ MoveGrammar:
 	db EXTREMESPEED
 	db 0 ; end set
 
-; 1: originally "[mon]の<LINE>[move]した！" ("[mon]<LINE>did [move]!")
+; 1: originally "[mon]は<LINE>[move]した！" ("[mon]<LINE>did [move]!")
 	db RECOVER
 	db TELEPORT
 	db BIDE
@@ -44,7 +44,7 @@ MoveGrammar:
 	db FLAIL
 	db 0 ; end set
 
-; 2: originally "[mon]の<LINE>[move]を　した！" ("[mon]<LINE>did [move]!")
+; 2: originally "[mon]は<LINE>[move]を　した！" ("[mon]<LINE>did [move]!")
 	db MEDITATE
 	db AGILITY
 	db MIMIC
