02e920ffd5e857715ddb9ef26879e4eeb1de82cf
SnorlaxMonster <snorlaxmonster@users.noreply.github.com>
2026-01-24 23:41:01 +1030

    Rescue dead Glitch City Forums URL

--- mobile/news/news.asm
+++ mobile/news/news.asm
@@ -1,4 +1,4 @@
-; http://forums.glitchcity.info/index.php?topic=7509.msg206449#msg206449
+; https://web.archive.org/web/20200414101940/https://forums.glitchcity.info/index.php?topic=7509.msg206449#msg206449
 
 	dab PlayersHouseDoll1Script ; related to "My Room" in Stadium 2?
 
