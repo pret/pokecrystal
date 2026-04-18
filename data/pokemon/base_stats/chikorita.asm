9b33b3ecc91ae630e0c3a46292eba4dccd6f8c49
jbg0707 <jonahbgottfried@gmail.com>
2026-04-18 14:56:20 -0400

    Replacing Chikorita with Crystal Onyx

--- data/pokemon/base_stats/chikorita.asm
+++ data/pokemon/base_stats/chikorita.asm
@@ -1,11 +1,11 @@
-	db CHIKORITA ; 152
+	db ONIX-C ; 152
 
-	db  145,  49,  65,  45,  49,  65
+	db  70,  90,  250,  140,  70,  160
 	;   hp  atk  def  spd  sat  sdf
 
-	db GRASS, NORMAL ; type
+	db ICE, DRAGON ; type
 	db 45 ; catch rate
-	db 64 ; base exp
+	db 108 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
@@ -13,7 +13,7 @@
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/chikorita/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_SLOW ; growth rate
+	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MONSTER, EGG_PLANT ; egg groups
 
 	; tm/hm learnset
